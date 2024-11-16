bb23dcb3fd23a70c633ae40380c96736c51ab511
vdraganic21 <vdraganic21@student.foi.hr>
2024-11-16 20:04:02 +0100

    Added Synergy packages. Removed demo css from project.

--- WebApp/indoor-localization/src/main.tsx
+++ WebApp/indoor-localization/src/main.tsx
@@ -3,6 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+import "@synergy-design-system/tokens/themes/light.css";
+import "@synergy-design-system/components/index.css";
+import "@synergy-design-system/styles/index.css";
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
