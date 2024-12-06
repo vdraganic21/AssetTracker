0ae36181e48829e0572c8da28da409390f8978a7
vdraganic21 <vdraganic21@student.foi.hr>
2024-12-06 22:04:13 +0100

    Side panel is now the right height.

--- WebApp/indoor-localization/src/components/Dashboard.tsx
+++ WebApp/indoor-localization/src/components/Dashboard.tsx
@@ -3,15 +3,11 @@ import SidePanelItem from "./SidePanelItem";
 
 function Dashboard() {
   return (
-    <>
-      <div className="content">
-        <div className="side-panel side-panel-border">
-          <SidePanelItem imageSrc={"/logo192.png"} name={"Assets"} />
-          <SidePanelItem imageSrc={"/logo192.png"} name={"Facilities"} />
-          <SidePanelItem imageSrc={"/logo192.png"} name={"Zones"} />
-        </div>
-      </div>
-    </>
+    <div className="side-panel side-panel-border">
+      <SidePanelItem imageSrc={"/logo192.png"} name={"Assets"} />
+      <SidePanelItem imageSrc={"/logo192.png"} name={"Facilities"} />
+      <SidePanelItem imageSrc={"/logo192.png"} name={"Zones"} />
+    </div>
   );
 }
 
