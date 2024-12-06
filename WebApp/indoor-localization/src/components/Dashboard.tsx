b69afc195d4461b9026de035f37ad8edabed3ddb
vdraganic21 <vdraganic21@student.foi.hr>
2024-12-06 19:05:20 +0100

    Added side panel UI element.

--- WebApp/indoor-localization/src/components/Dashboard.tsx
+++ WebApp/indoor-localization/src/components/Dashboard.tsx
@@ -1,8 +1,15 @@
+import "./Dashboard.css";
+import SidePanelItem from "./SidePanelItem";
+
 function Dashboard() {
   return (
     <>
       <div className="content">
-        <span className="syn-heading--3x-large">Dashboard</span>
+        <div className="side-panel side-panel-border">
+          <SidePanelItem imageSrc={"/logo192.png"} name={"Assets"} />
+          <SidePanelItem imageSrc={"/logo192.png"} name={"Facilities"} />
+          <SidePanelItem imageSrc={"/logo192.png"} name={"Zones"} />
+        </div>
       </div>
     </>
   );
