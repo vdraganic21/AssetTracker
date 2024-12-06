58eb5c4c348882b2580880d6a0e3e9f319e90a6a
vdraganic21 <vdraganic21@student.foi.hr>
2024-12-06 23:43:31 +0100

    Added side panel content formatting.

--- WebApp/indoor-localization/src/components/Dashboard.tsx
+++ WebApp/indoor-localization/src/components/Dashboard.tsx
@@ -3,10 +3,13 @@ import SidePanelItem from "./SidePanelItem";
 
 function Dashboard() {
   return (
-    <div className="side-panel side-panel-border">
-      <SidePanelItem imageSrc={"/logo192.png"} name={"Assets"} />
-      <SidePanelItem imageSrc={"/logo192.png"} name={"Facilities"} />
-      <SidePanelItem imageSrc={"/logo192.png"} name={"Zones"} />
+    <div className="side-panel-container">
+      <div className="side-panel-bar side-panel-border">
+        <SidePanelItem imageSrc={"/logo192.png"} name={"Assets"} />
+        <SidePanelItem imageSrc={"/logo192.png"} name={"Facilities"} />
+        <SidePanelItem imageSrc={"/logo192.png"} name={"Zones"} />
+      </div>
+      <div className="side-panel-content side-panel-border">content</div>
     </div>
   );
 }
