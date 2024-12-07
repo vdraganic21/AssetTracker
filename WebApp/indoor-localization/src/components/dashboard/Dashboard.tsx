ba94aac0e4b5b260cffa6123d69c05c3c12b0169
vdraganic21 <vdraganic21@student.foi.hr>
2024-12-07 20:05:35 +0100

    Added FloorMapDisplay component.

--- WebApp/indoor-localization/src/components/dashboard/Dashboard.tsx
+++ WebApp/indoor-localization/src/components/dashboard/Dashboard.tsx
@@ -1,8 +1,14 @@
 import "./Dashboard.css";
 import DashboardSidePanel from "./DashboardSidePanel";
+import FloorMapDisplay from "./FloorMapDisplay";
 
 function Dashboard() {
-  return <DashboardSidePanel></DashboardSidePanel>;
+  return (
+    <div className="content">
+      <DashboardSidePanel></DashboardSidePanel>
+      <FloorMapDisplay></FloorMapDisplay>
+    </div>
+  );
 }
 
 export default Dashboard;
---
+++ WebApp/indoor-localization/src/components/dashboard/FloorMapDisplay.tsx
@@ -0,0 +1,11 @@
+import "./FloorMapDisplay.css";
+
+function FloorMapDisplay() {
+  return (
+    <div className="floor-map-display">
+      <canvas></canvas>
+    </div>
+  );
+}
+
+export default FloorMapDisplay;
