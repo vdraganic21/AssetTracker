bb86697c9dcb0681671c66e8ef55593221815a10
vdraganic21 <vdraganic21@student.foi.hr>
2024-12-08 01:41:55 +0100

    Added asset list item component.

---
+++ Android/indoor_localization/lib/presentation/assets_page/asset_list_item.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:indoor_localization/entities/asset.dart';
+
+class AssetListItem extends StatelessWidget {
+  final Asset asset;
+
+  const AssetListItem({super.key, required this.asset});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(8.0),
+      margin: const EdgeInsets.symmetric(vertical: 4.0),
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey, width: 1.0),
+        borderRadius: BorderRadius.circular(5.0),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: Text(
+              asset.name,
+              style: const TextStyle(
+                fontSize: 16.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: Text(
+              asset.floorMap,
+              style: const TextStyle(
+                fontSize: 16.0,
+                color: Colors.blue,
+              ),
+            ),
+          ),
+        ],
+
+      ),
+    );
+  }
+}
