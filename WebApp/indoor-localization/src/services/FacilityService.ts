16d8595d5cf73bf1d882ffa455060cb8a9e7a149
vdraganic21 <vdraganic21@student.foi.hr>
2024-12-12 20:47:16 +0100

    Fixed facility service.

--- WebApp/indoor-localization/src/services/FacilityService.ts
+++ WebApp/indoor-localization/src/services/FacilityService.ts
@@ -1,50 +1,58 @@
-import { Zone } from "../entities/Zone";
-import { Asset } from "../entities/Asset";
 import { Facility } from "../entities/Facility";
+import { Asset } from "../entities/Asset";
+import { Zone } from "../entities/Zone";
 import { EntityService } from "./Service";
 
-export class ZoneService extends EntityService {
+export class FacilityService extends EntityService {
 
-  static Get(id: number): Zone | null {
-    return this.zoneRepo.Get(id);
+  static Get(id: number): Facility | null {
+    return this.facilityRepo.Get(id);
   }
 
-  static GetAll(): Zone[] {
-    return this.zoneRepo.GetAll();
+  static GetAll(): Facility[] {
+    return this.facilityRepo.GetAll();
   }
 
-  static Add(zone: Zone): boolean {
-    return this.zoneRepo.Add(zone);
+  static Add(facility: Facility): boolean {
+    return this.facilityRepo.Add(facility);
   }
 
   static Delete(id: number): boolean {
-    return this.zoneRepo.Delete(id);
+    return this.facilityRepo.Delete(id);
   }
 
-  static Update(zone: Zone): boolean {
-    return this.zoneRepo.Update(zone);
+  static Update(facility: Facility): boolean {
+    return this.facilityRepo.Update(facility);
   }
 
-  static GetZoneParentFacility(id: number): Facility | null {
-    const zone = this.zoneRepo.Get(id);
-    if (zone) {
-      return this.facilityRepo.Get(zone.parentFacilityId);
-    }
-    return null;
-  }
-
-  static GetAssetsInZone(id: number): Asset[] {
-    const zone = this.zoneRepo.Get(id);
-    if (zone) {
-      return zone.containedAssetsIds.map(assetId => this.assetRepo.Get(assetId)).filter(asset => asset !== null) as Asset[];
+  static GetAssetsInFacility(id: number): Asset[] {
+    const facility = this.facilityRepo.Get(id);
+    if (facility) {
+      return facility.containedAssetsIds.map(assetId => this.assetRepo.Get(assetId)).filter(asset => asset !== null) as Asset[];
     }
     return [];
   }
 
-  static ZoneContainsAsset(zoneId: number, assetId: number): boolean {
-    const zone = this.zoneRepo.Get(zoneId);
-    if (zone) {
-      return zone.containedAssetsIds.includes(assetId);
+  static GetZonesInFacility(id: number): Zone[] {
+    const facility = this.facilityRepo.Get(id);
+    if (facility) {
+      return facility.containedZonesIds.map(zoneId => this.zoneRepo.Get(zoneId)).filter(zone => zone !== null) as Zone[];
+    }
+    return [];
+  }
+
+  static FacilityContainsAsset(facilityId: number, assetId: number): boolean {
+    const facility = this.facilityRepo.Get(facilityId);
+    if (facility) {
+      return facility.containedAssetsIds.includes(assetId);
+    }
+    return false;
+  }
+
+  static FacilityContainsZone(facilityId: number, zoneId: number): boolean {
+    const facility = this.facilityRepo.Get(facilityId);
+    if (facility) {
+      return facility.containedZonesIds.includes(zoneId);
     }
     return false;
   }
