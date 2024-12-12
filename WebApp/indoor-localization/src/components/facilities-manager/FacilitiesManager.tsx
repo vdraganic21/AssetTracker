8cfed805897d3ae4df0d99270f7b34047c99a03c
aslavicek21 <aslavicek21@foi.hr>
2024-12-12 22:49:13 +0100

    Added search bar and buttons (delete/add) in FacilitiesManager.

--- WebApp/indoor-localization/src/components/facilities-manager/FacilitiesManager.tsx
+++ WebApp/indoor-localization/src/components/facilities-manager/FacilitiesManager.tsx
@@ -1,5 +1,13 @@
 import Footer from "../Footer";
 import "../Manager.css";
+import {
+  SynButton,
+  SynCheckbox,
+  SynDivider,
+  SynInput,
+  SynOption,
+  SynSelect,
+} from "@synergy-design-system/react";
 import FacilityCardContainer from "./FacilityCardContainer";
 
 const cards = [
@@ -28,6 +36,11 @@ const cards = [
     imageAlt: "card image",
     facilityName: "Warehouse 5",
   },
+  {
+    imageSrc: "/floorMapDemo.png",
+    imageAlt: "card image",
+    facilityName: "Warehouse 5",
+  },
 ];
 
 const sortOptions = [
@@ -39,7 +52,31 @@ function FacilitiesManager() {
   return (
     <>
       <div className="content content-border">
-        <span className="syn-heading--3x-large">Facilities</span>
+        <div className="header-row">
+          <span className="syn-heading--3x-large">Facilities</span>
+          <div className="button-group">
+            <SynButton variant="outline">Delete</SynButton>
+            <SynButton variant="filled" className="syn-border-radius-medium">
+              Add
+            </SynButton>
+          </div>
+        </div>
+        <SynDivider className="content-divider" />
+        <div className="search-row">
+          <SynInput className="search-input" placeholder="Search" />
+          <p>Sort by:</p>
+          <SynSelect value={sortOptions[0]?.value} className="sort-select">
+            {sortOptions.map((sortOption, index) => (
+              <SynOption
+                tabIndex={index}
+                selected={0 == index}
+                value={sortOption.value}
+              >
+                {sortOption.name}
+              </SynOption>
+            ))}
+          </SynSelect>
+        </div>
         <FacilityCardContainer cards={cards}></FacilityCardContainer>
       </div>
       <Footer />
