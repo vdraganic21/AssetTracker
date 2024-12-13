a0ebe1fc0fbcc6c8f622902f1d65e59eb998dc3a
vdraganic21 <vdraganic21@student.foi.hr>
2024-12-13 13:16:27 +0100

    Implemented repository interface.

---
+++ Android/indoor_localization/lib/repository-interfaces/repository.dart
@@ -0,0 +1,25 @@
+abstract class IRepository<T> {
+  /// Gets a single entity by its ID.
+  /// @param id - The ID of the entity.
+  /// @returns The entity of type T.
+  T? get(int id);
+
+  /// Gets all entities.
+  /// @returns A list of entities of type T.
+  List<T> getAll();
+
+  /// Adds a new entity.
+  /// @param entity - The entity to add.
+  /// @returns A boolean indicating whether the operation was successful.
+  bool add(T entity);
+
+  /// Deletes an entity by its ID.
+  /// @param id - The ID of the entity to delete.
+  /// @returns A boolean indicating whether the operation was successful.
+  bool delete(int id);
+
+  /// Updates an existing entity.
+  /// @param entity - The entity to update.
+  /// @returns A boolean indicating whether the operation was successful.
+  bool update(T entity);
+}
