c61b5577f47d7df338d1a986dee139a3c4e2bffc
vdraganic21 <vdraganic21@student.foi.hr>
2024-12-21 16:46:48 +0100

    Implemented asset search functionality.

--- WebApp/indoor-localization/src/components/dashboard/side_panel_content/AssetsSidePanel.tsx
+++ WebApp/indoor-localization/src/components/dashboard/side_panel_content/AssetsSidePanel.tsx
@@ -3,6 +3,8 @@ import { Asset } from "../../../entities/Asset";
 import { Point } from "../../../entities/Point";
 import "./AssetsSidePanel.css";
 import AssetsSidePaneList from "./AssetsSidePanelList";
+import { SynInputEvent } from "@synergy-design-system/react/components/checkbox.js";
+import { useEffect, useState } from "react";
 
 function AssetsSidePanel() {
   //const assets = AssetService.GetAll();
@@ -14,10 +16,33 @@ function AssetsSidePanel() {
     new Asset(1, "Asset5", 1, new Point(1, 1), new Date(), true, []),
   ];
 
+  const [searchTerm, setSearchTerm] = useState("");
+  const [filteredAssets, setFilteredAssets] = useState(assets);
+
+  const handleSearch = (event: SynInputEvent) => {
+    const term = (event.target as HTMLInputElement).value;
+    setSearchTerm(term);
+  };
+
+  useEffect(() => {
+    let filtered = assets.filter((asset) =>
+      asset.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+    filtered = filtered.sort((a, b) => a.name.localeCompare(b.name));
+
+    setFilteredAssets(filtered);
+  }, [searchTerm]);
+
   return (
     <div>
-      <SynInput className="side-panel-asset-search" placeholder="Search" />
-      <AssetsSidePaneList assets={assets} />
+      <SynInput
+        className="side-panel-asset-search"
+        placeholder="Search"
+        value={searchTerm}
+        onSynInput={handleSearch}
+      />
+      <AssetsSidePaneList assets={filteredAssets} />
     </div>
   );
 }
