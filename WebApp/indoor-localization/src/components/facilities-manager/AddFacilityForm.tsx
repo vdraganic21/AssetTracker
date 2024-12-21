ec8e339b8c278211b1f9d80a0c1080b08606431f
vdraganic21 <vdraganic21@student.foi.hr>
2024-12-21 12:28:45 +0100

    Added input fields. Added Form css file.

--- WebApp/indoor-localization/src/components/facilities-manager/AddFacilityForm.tsx
+++ WebApp/indoor-localization/src/components/facilities-manager/AddFacilityForm.tsx
@@ -1,10 +1,15 @@
+import { SynDivider, SynFile, SynInput } from "@synergy-design-system/react";
 import Footer from "../Footer";
+import "../Form.css";
 
 function AddFacilityForm() {
   return (
     <>
       <div className="content content-border">
         <span className="syn-heading--3x-large">Add facility</span>
+        <SynDivider className="content-divider" />
+        <SynInput name="name" label="Name"></SynInput>
+        <SynFile label="Floor map image" className="form-top-margin"></SynFile>
       </div>
       <Footer />
     </>
