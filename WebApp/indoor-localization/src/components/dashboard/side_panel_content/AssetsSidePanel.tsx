4fbaf7a646b9c3df6bd2540be287e76d29d2729d
vdraganic21 <vdraganic21@student.foi.hr>
2024-12-21 16:30:48 +0100

    Implemented basic assets side panel UI.

--- WebApp/indoor-localization/src/components/dashboard/side_panel_content/AssetsSidePanel.tsx
+++ WebApp/indoor-localization/src/components/dashboard/side_panel_content/AssetsSidePanel.tsx
@@ -1,5 +1,32 @@
+import { SynDivider, SynInput } from "@synergy-design-system/react";
+import { Asset } from "../../../entities/Asset";
+import { Point } from "../../../entities/Point";
+import "./AssetsSidePanel.css";
+
 function AssetsSidePanel() {
-  return <div>assets</div>;
+  //const assets = AssetService.GetAll();
+  const assets = [
+    new Asset(1, "Asset1", 1, new Point(1, 1), new Date(), true, []),
+    new Asset(1, "Asset2", 1, new Point(1, 1), new Date(), true, []),
+    new Asset(1, "Asset3", 1, new Point(1, 1), new Date(), true, []),
+    new Asset(1, "Asset4", 1, new Point(1, 1), new Date(), true, []),
+    new Asset(1, "Asset5", 1, new Point(1, 1), new Date(), true, []),
+  ];
+
+  return (
+    <div>
+      <SynInput className="side-panel-asset-search" placeholder="Search" />
+      <SynDivider />
+      {assets.map((asset, index) => (
+        <>
+          <p className="side-panel-asset-list" key={index}>
+            {asset.name}
+          </p>
+          <SynDivider />
+        </>
+      ))}
+    </div>
+  );
 }
 
 export default AssetsSidePanel;
