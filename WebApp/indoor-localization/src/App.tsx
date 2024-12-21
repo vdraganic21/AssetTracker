a46a29607a3da8acc5d14d4df61328c248f49849
vdraganic21 <vdraganic21@student.foi.hr>
2024-12-21 12:05:57 +0100

    Removed unused import.

--- WebApp/indoor-localization/src/App.tsx
+++ WebApp/indoor-localization/src/App.tsx
@@ -2,7 +2,6 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header";
 import Dashboard from "./components/dashboard/Dashboard";
-import Reports from "./components/Reports";
 import FacilitiesManager from "./components/facilities-manager/FacilitiesManager";
 import ReportsPage from "./components/reports-page/ReportsPage";
 import AssetsManager from "./components/AssetsManager";
