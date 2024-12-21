f442c0b7d6f90b8903b9f502690a277b6c66d0a1
vdraganic21 <vdraganic21@student.foi.hr>
2024-12-21 16:53:19 +0100

    Assets now fetched from AssetsService.

--- WebApp/indoor-localization/src/components/dashboard/side_panel_content/AssetsSidePanel.tsx
+++ WebApp/indoor-localization/src/components/dashboard/side_panel_content/AssetsSidePanel.tsx
@@ -1,20 +1,12 @@
 import { SynInput } from "@synergy-design-system/react";
-import { Asset } from "../../../entities/Asset";
-import { Point } from "../../../entities/Point";
 import "./AssetsSidePanel.css";
 import AssetsSidePaneList from "./AssetsSidePanelList";
 import { SynInputEvent } from "@synergy-design-system/react/components/checkbox.js";
 import { useEffect, useState } from "react";
+import { AssetService } from "../../../services/AssetService";
 
 function AssetsSidePanel() {
-  //const assets = AssetService.GetAll();
-  const assets = [
-    new Asset(1, "Asset1", 1, new Point(1, 1), new Date(), true, []),
-    new Asset(1, "Asset2", 1, new Point(1, 1), new Date(), true, []),
-    new Asset(1, "Asset3", 1, new Point(1, 1), new Date(), true, []),
-    new Asset(1, "Asset4", 1, new Point(1, 1), new Date(), true, []),
-    new Asset(1, "Asset5", 1, new Point(1, 1), new Date(), true, []),
-  ];
+  const assets = AssetService.GetAll();
 
   const [searchTerm, setSearchTerm] = useState("");
   const [filteredAssets, setFilteredAssets] = useState(assets);
