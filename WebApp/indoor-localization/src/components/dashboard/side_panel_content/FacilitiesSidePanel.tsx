4099cfa5354b44add6331889e0856c0f401a6711
vdraganic21 <vdraganic21@student.foi.hr>
2024-12-23 15:20:48 +0100

    Implemented facilities side panel UI.

--- WebApp/indoor-localization/src/components/dashboard/side_panel_content/FacilitiesSidePanel.tsx
+++ WebApp/indoor-localization/src/components/dashboard/side_panel_content/FacilitiesSidePanel.tsx
@@ -1,5 +1,18 @@
+import { SynInput } from "@synergy-design-system/react";
+import FacilityCardContainer from "../../facilities-manager/FacilityCardContainer";
+import { FacilityService } from "../../../services/FacilityService";
+
 function FacilitiesSidePanel() {
-  return <div>facilities</div>;
+  const facilities = FacilityService.GetAll();
+
+  return (
+    <>
+      <SynInput className="side-panel-asset-search" placeholder="Search" />
+      <div className="scrollable-list">
+        <FacilityCardContainer facilities={facilities} />
+      </div>
+    </>
+  );
 }
 
 export default FacilitiesSidePanel;
