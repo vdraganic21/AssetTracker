af0d8ee541fbea5d824a07b574aeca01272cb037
vdraganic21 <vdraganic21@student.foi.hr>
2024-12-23 15:27:56 +0100

    Implemented search function to facilities side panel

--- WebApp/indoor-localization/src/components/dashboard/side_panel_content/FacilitiesSidePanel.tsx
+++ WebApp/indoor-localization/src/components/dashboard/side_panel_content/FacilitiesSidePanel.tsx
@@ -1,15 +1,38 @@
 import { SynInput } from "@synergy-design-system/react";
 import FacilityCardContainer from "../../facilities-manager/FacilityCardContainer";
 import { FacilityService } from "../../../services/FacilityService";
+import { useEffect, useState } from "react";
+import { SynInputEvent } from "@synergy-design-system/react/components/checkbox.js";
 
 function FacilitiesSidePanel() {
   const facilities = FacilityService.GetAll();
 
+  const [searchTerm, setSearchTerm] = useState("");
+  const [filteredFacilities, setFilteredFacilities] = useState(facilities);
+
+  const handleSearch = (event: SynInputEvent) => {
+    const term = (event.target as HTMLInputElement).value;
+    setSearchTerm(term);
+  };
+
+  useEffect(() => {
+    let filtered = facilities.filter((facility) =>
+      facility.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+    filtered = filtered.sort((a, b) => a.name.localeCompare(b.name));
+
+    setFilteredFacilities(filtered);
+  }, [searchTerm]);
   return (
     <>
-      <SynInput className="side-panel-asset-search" placeholder="Search" />
+      <SynInput
+        className="side-panel-asset-search"
+        placeholder="Search"
+        onSynInput={handleSearch}
+      />
       <div className="scrollable-list">
-        <FacilityCardContainer facilities={facilities} />
+        <FacilityCardContainer facilities={filteredFacilities} />
       </div>
     </>
   );
