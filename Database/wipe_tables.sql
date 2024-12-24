77254223a5e622ce8a9acbd7dec84dc40f144f16
lbrlekovi20 <lbrlekovi20@student.foi.hr>
2024-12-24 03:19:05 +0100

    Created scripts for setting up the database, filling the database with tables, filling the tables with data and removing all data from the tables.

---
+++ Database/wipe_tables.sql
@@ -0,0 +1,18 @@
+-- Disable foreign key constraints temporarily
+SET session_replication_role = 'replica';
+
+-- Delete data in reverse dependency order to handle foreign key constraints
+DELETE FROM assetZoneHistory;
+DELETE FROM assetPositionHistory;
+DELETE FROM zones;
+DELETE FROM assets;
+DELETE FROM floorMaps;
+
+SELECT setval(pg_get_serial_sequence('assetZoneHistory', 'id'), 1, false);
+SELECT setval(pg_get_serial_sequence('assetPositionHistory', 'id'), 1, false);
+SELECT setval(pg_get_serial_sequence('zones', 'id'), 1, false);
+SELECT setval(pg_get_serial_sequence('assets', 'id'), 1, false);
+SELECT setval(pg_get_serial_sequence('floorMaps', 'id'), 1, false);
+
+-- Re-enable foreign key constraints
+SET session_replication_role = 'origin';
