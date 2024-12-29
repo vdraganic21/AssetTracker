9f8b4b5bc586b9b7ad242ad93c03c22c4151a47e
vdraganic21 <vdraganic21@student.foi.hr>
2024-12-29 21:06:15 +0100

    Added EditFacilityForm component.

---
+++ WebApp/indoor-localization/src/components/facilities-manager/EditFacilityForm.tsx
@@ -0,0 +1,142 @@
+import {
+	SynButton,
+	SynDivider,
+	SynFile,
+	SynInput,
+} from "@synergy-design-system/react";
+import Footer from "../Footer";
+import "../Form.css";
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import {
+	SynInputEvent,
+	SynChangeEvent,
+} from "@synergy-design-system/react/components/checkbox.js";
+import { FacilityService } from "../../services/FacilityService";
+
+function EditFacilityForm() {
+	const navigate = useNavigate();
+	const { id } = useParams();
+	const facilityId = parseInt(id ?? "0", 10);
+	const facility = FacilityService.Get(facilityId);
+
+	useEffect(() => {
+		if (facility == null) navigate("/facilities");
+	}, [facility, navigate]);
+
+	const [facilityName, setFacilityName] = useState(facility?.name ?? "");
+	const [file, setFile] = useState<File | null>(null);
+	const [fileError, setFileError] = useState("");
+	const [previewSrc, setPreviewSrc] = useState<string | null>(
+		facility?.imageBase64 ?? null
+	);
+	const [isFormValid, setIsFormValid] = useState(false);
+
+	const handleCancel = () => {
+		navigate("/facilities");
+	};
+
+	const handleNameChange = (e: SynInputEvent) => {
+		const facilityName = (e.target as HTMLInputElement).value;
+		setFacilityName(facilityName);
+		validateForm(facilityName, file);
+	};
+
+	const handleFileChange = (e: SynChangeEvent) => {
+		const selectedFile = (e.target as HTMLInputElement).files?.[0];
+
+		if (selectedFile == null) {
+			setFileError("No file selected.");
+			setFile(null);
+			setPreviewSrc(null);
+		} else if (selectedFile && selectedFile.type !== "image/png") {
+			setFileError("Only .png files are allowed.");
+			setFile(null);
+			setPreviewSrc(null);
+		} else {
+			setFileError("");
+			setFile(selectedFile);
+
+			const reader = new FileReader();
+			reader.onload = () => {
+				setPreviewSrc(reader.result as string);
+			};
+			reader.readAsDataURL(selectedFile);
+		}
+
+		validateForm(facilityName, selectedFile ?? null);
+	};
+
+	const validateForm = (name: string, file: File | null) => {
+		setIsFormValid(
+			!!name.trim() &&
+				(previewSrc == facility?.imageBase64 || (file !== null && !fileError))
+		);
+	};
+
+	const handleEdit = () => {
+		if (!facility) return;
+
+		if (facilityName.trim() !== "") facility.name = facilityName;
+
+		if (file) {
+			const reader = new FileReader();
+			reader.onload = () => {
+				const base64ImageString = reader.result as string;
+				facility.imageBase64 = base64ImageString;
+			};
+			reader.readAsDataURL(file);
+		}
+
+		let isUpdated = FacilityService.Update(facility);
+
+		if (isUpdated) {
+			navigate("/facilities");
+		}
+	};
+
+	return (
+		<>
+			<div className="content content-border">
+				<span className="syn-heading--3x-large">Edit facility</span>
+				<SynDivider className="content-divider" />
+				<SynInput
+					name="name"
+					label="Name"
+					className="form-input-width"
+					value={facilityName}
+					onSynInput={handleNameChange}
+				/>
+				<SynFile
+					label="Floor map image"
+					className="form-top-margin"
+					onSynChange={handleFileChange}
+				/>
+				{fileError && <p style={{ color: "red" }}>{fileError}</p>}
+				{previewSrc && (
+					<img
+						src={previewSrc}
+						className="image-preview"
+						alt="Selected floor map"
+					/>
+				)}
+				<div className="form-button-container">
+					<SynButton className="form-button" onClick={handleCancel}>
+						Cancel
+					</SynButton>
+					<SynButton
+						variant="filled"
+						className="form-button"
+						disabled={!isFormValid}
+						onClick={handleEdit}
+					>
+						Edit
+					</SynButton>
+				</div>
+			</div>
+			<Footer />
+		</>
+	);
+}
+
+export default EditFacilityForm;
