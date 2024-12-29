e72cb7f95a1fc65c451484aba0fa22e9eda730d8
vdraganic21 <vdraganic21@student.foi.hr>
2024-12-29 19:49:16 +0100

    Added edit button to facility card component.

--- WebApp/indoor-localization/src/components/facilities-manager/FacilityCard.tsx
+++ WebApp/indoor-localization/src/components/facilities-manager/FacilityCard.tsx
@@ -1,13 +1,25 @@
 import "./FacilityCard.css";
 import { Facility } from "../../entities/Facility";
+import { useNavigate } from "react-router-dom";
+import { SynIcon } from "@synergy-design-system/react";
 
 function FacilityCard({ facility }: { facility: Facility }) {
+	const navigate = useNavigate();
+
 	return (
 		<div className="facility-card">
 			<div className="facility-card-image">
 				<img src={facility.imageBase64} alt={facility.name + "floor map"} />
 			</div>
 			<div className="facility-card-title">{facility.name}</div>
+			<SynIcon
+				library="fa"
+				name="far-edit"
+				className="facility-edit-icon"
+				onClick={() => {
+					navigate(`/facilities/edit/${facility.id}`);
+				}}
+			/>
 		</div>
 	);
 }
