2ba113a280f8ee6f24884d0e2c88e70ea23af29d
vdraganic21 <vdraganic21@student.foi.hr>
2024-12-30 19:59:42 +0100

    Updated React version. Added Konva library.

--- WebApp/indoor-localization/src/components/dashboard/FloorMapDisplay.tsx
+++ WebApp/indoor-localization/src/components/dashboard/FloorMapDisplay.tsx
@@ -2,48 +2,54 @@ import { useEffect, useRef } from "react";
 import DashboardDisplayManager from "./../../managers/DashboardDisplayManager.tsx"; // Import the manager
 import "./FloorMapDisplay.css";
 import { SynButton, SynIcon } from "@synergy-design-system/react";
+import { Stage, Layer, Rect, Circle } from "react-konva";
 
 function FloorMapDisplay() {
-  const canvasRef = useRef<HTMLCanvasElement>(null);
-  const displayManagerRef = useRef<DashboardDisplayManager | null>(null);
+	const canvasRef = useRef<HTMLCanvasElement>(null);
+	const displayManagerRef = useRef<DashboardDisplayManager | null>(null);
 
-  useEffect(() => {
-    const canvas = canvasRef.current;
-    if (canvas) {
-      const manager = new DashboardDisplayManager(canvas);
-      displayManagerRef.current = manager;
+	useEffect(() => {
+		const canvas = canvasRef.current;
+		if (canvas) {
+			const manager = new DashboardDisplayManager(canvas);
+			displayManagerRef.current = manager;
 
-      return () => {
-        manager.destroy();
-      };
-    }
-  }, []);
+			return () => {
+				manager.destroy();
+			};
+		}
+	}, []);
 
-  return (
-    <div className="floor-map-display">
-      <canvas ref={canvasRef}></canvas>
-      <div className="bottom-right-container">
-        <div className="facility-column">
-          <p className="facility-name">Facility name</p>
-        </div>
-        <div className="buttons-column">
-          <SynButton>
-            <SynIcon
-              library="fa"
-              name="fas-rotate-right"
-              className="button-icon"
-            />
-          </SynButton>
-          <SynButton>
-            <SynIcon library="fa" name="fas-plus" className="button-icon" />
-          </SynButton>
-          <SynButton>
-            <SynIcon library="fa" name="fas-minus" className="button-icon" />
-          </SynButton>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="floor-map-display">
+			<Stage width={window.innerWidth} height={window.innerHeight}>
+				<Layer>
+					<Rect width={50} height={50} fill="red" />
+					<Circle x={200} y={200} stroke="black" radius={50} />
+				</Layer>
+			</Stage>
+			<div className="bottom-right-container">
+				<div className="facility-column">
+					<p className="facility-name">Facility name</p>
+				</div>
+				<div className="buttons-column">
+					<SynButton>
+						<SynIcon
+							library="fa"
+							name="fas-rotate-right"
+							className="button-icon"
+						/>
+					</SynButton>
+					<SynButton>
+						<SynIcon library="fa" name="fas-plus" className="button-icon" />
+					</SynButton>
+					<SynButton>
+						<SynIcon library="fa" name="fas-minus" className="button-icon" />
+					</SynButton>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default FloorMapDisplay;
