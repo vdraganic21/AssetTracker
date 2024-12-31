e949ddbf47f5c821a7477440b17c900fbd7c0bde
vdraganic21 <vdraganic21@student.foi.hr>
2024-12-31 13:47:17 +0100

    Dashboard display is now responsive to window size changes.

--- WebApp/indoor-localization/src/components/dashboard/FloorMapDisplay.tsx
+++ WebApp/indoor-localization/src/components/dashboard/FloorMapDisplay.tsx
@@ -1,21 +1,45 @@
+import Konva from "konva";
 import "./FloorMapDisplay.css";
 import { SynButton, SynIcon } from "@synergy-design-system/react";
+import { useEffect, useRef } from "react";
 import { Stage, Layer, Image } from "react-konva";
 import useImage from "use-image";
 
 function FloorMapDisplay() {
+	const stageRef = useRef<Konva.Stage>(null);
+	const containerRef = useRef(null);
+
 	const FloorMapImage = () => {
 		const [image] = useImage("/floorMapDemo.png");
 		return <Image image={image} />;
 	};
 
+	useEffect(() => {
+		const resizeStage = () => {
+			if (stageRef.current && containerRef.current) {
+				const { offsetWidth, offsetHeight } = containerRef.current;
+				stageRef.current.width(offsetWidth);
+				stageRef.current.height(offsetHeight);
+			}
+		};
+
+		window.addEventListener("resize", resizeStage);
+		resizeStage();
+
+		return () => {
+			window.removeEventListener("resize", resizeStage);
+		};
+	}, []);
+
 	return (
 		<div className="floor-map-display">
-			<Stage width={window.innerWidth} height={window.innerHeight}>
-				<Layer draggable={true}>
-					<FloorMapImage />
-				</Layer>
-			</Stage>
+			<div className="konva-stage-container" ref={containerRef}>
+				<Stage ref={stageRef}>
+					<Layer>
+						<FloorMapImage />
+					</Layer>
+				</Stage>
+			</div>
 			<div className="bottom-right-container">
 				<div className="facility-column">
 					<p className="facility-name">Facility name</p>
