965bb4159875d84406ab728fcf3c243c45537167
vdraganic21 <vdraganic21@student.foi.hr>
2025-01-03 15:35:05 +0100

    Added function that gets the currently selected facility. UI changes if no facility is provided.

--- WebApp/indoor-localization/src/components/dashboard/Dashboard.tsx
+++ WebApp/indoor-localization/src/components/dashboard/Dashboard.tsx
@@ -1,17 +1,47 @@
-import { Facility } from "../../entities/Facility";
+import CookieService from "../../services/CookieService";
+import { FacilityService } from "../../services/FacilityService";
 import "./Dashboard.css";
 import DashboardSidePanel from "./DashboardSidePanel";
 import FloorMapDisplay from "./FloorMapDisplay";
 
 function Dashboard() {
+	function getSelectedFaciltiyDefault() {
+		let selectedFacility = FacilityService.GetAll()[0];
+		if (!selectedFacility) {
+			CookieService.delete("selectedFacility");
+			return null;
+		}
+
+		CookieService.set("selectedFacility", selectedFacility.id.toString());
+		return selectedFacility;
+	}
+
 	function getSelectedFacility() {
-		return new Facility(1, "Warehouse", "/floorMapDemo.png");
+		let selectedFacilityId = CookieService.get("selectedFacility");
+		let selectedFacility;
+
+		if (!selectedFacilityId) {
+			return getSelectedFaciltiyDefault();
+		}
+
+		selectedFacility = FacilityService.Get(parseInt(selectedFacilityId));
+		if (!selectedFacility) {
+			return getSelectedFaciltiyDefault();
+		}
+
+		return selectedFacility;
+	}
+
+	const selectedFacility = getSelectedFacility();
+
+	if (!selectedFacility) {
+		return <div className="content">No facility found.</div>;
 	}
 
 	return (
 		<div className="content">
 			<DashboardSidePanel></DashboardSidePanel>
-			<FloorMapDisplay facility={getSelectedFacility()}></FloorMapDisplay>
+			<FloorMapDisplay facility={selectedFacility}></FloorMapDisplay>
 		</div>
 	);
 }
