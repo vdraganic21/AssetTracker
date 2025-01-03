07446ebb6baa73c9af8dbb26cd644aa8b42fc2d4
vdraganic21 <vdraganic21@student.foi.hr>
2025-01-03 15:39:16 +0100

    Refactored getSelectedFacilityFunctions into separate service class.

--- WebApp/indoor-localization/src/components/dashboard/Dashboard.tsx
+++ WebApp/indoor-localization/src/components/dashboard/Dashboard.tsx
@@ -1,38 +1,10 @@
-import CookieService from "../../services/CookieService";
-import { FacilityService } from "../../services/FacilityService";
+import SelectedFacilityService from "../../services/SelectedFacilityService";
 import "./Dashboard.css";
 import DashboardSidePanel from "./DashboardSidePanel";
 import FloorMapDisplay from "./FloorMapDisplay";
 
 function Dashboard() {
-	function getSelectedFaciltiyDefault() {
-		let selectedFacility = FacilityService.GetAll()[0];
-		if (!selectedFacility) {
-			CookieService.delete("selectedFacility");
-			return null;
-		}
-
-		CookieService.set("selectedFacility", selectedFacility.id.toString());
-		return selectedFacility;
-	}
-
-	function getSelectedFacility() {
-		let selectedFacilityId = CookieService.get("selectedFacility");
-		let selectedFacility;
-
-		if (!selectedFacilityId) {
-			return getSelectedFaciltiyDefault();
-		}
-
-		selectedFacility = FacilityService.Get(parseInt(selectedFacilityId));
-		if (!selectedFacility) {
-			return getSelectedFaciltiyDefault();
-		}
-
-		return selectedFacility;
-	}
-
-	const selectedFacility = getSelectedFacility();
+	const selectedFacility = SelectedFacilityService.getSelectedFacility();
 
 	if (!selectedFacility) {
 		return <div className="content">No facility found.</div>;
---
+++ WebApp/indoor-localization/src/services/SelectedFacilityService.ts
@@ -0,0 +1,31 @@
+import CookieService from "./CookieService";
+import { FacilityService } from "./FacilityService";
+
+export default class SelectedFacilityService {
+    private static getSelectedFaciltiyDefault() {
+		let selectedFacility = FacilityService.GetAll()[0];
+		if (!selectedFacility) {
+			CookieService.delete("selectedFacility");
+			return null;
+		}
+
+		CookieService.set("selectedFacility", selectedFacility.id.toString());
+		return selectedFacility;
+	}
+
+	static getSelectedFacility() {
+		let selectedFacilityId = CookieService.get("selectedFacility");
+		let selectedFacility;
+
+		if (!selectedFacilityId) {
+			return this.getSelectedFaciltiyDefault();
+		}
+
+		selectedFacility = FacilityService.Get(parseInt(selectedFacilityId));
+		if (!selectedFacility) {
+			return this.getSelectedFaciltiyDefault();
+		}
+
+		return selectedFacility;
+	}
+}
