b403b0b1d83435a34789738e6e96f2a4858759ff
vdraganic21 <vdraganic21@student.foi.hr>
2025-01-03 16:07:40 +0100

    Refactored dashboard to use FullPageNotification.

--- WebApp/indoor-localization/src/components/dashboard/Dashboard.tsx
+++ WebApp/indoor-localization/src/components/dashboard/Dashboard.tsx
@@ -1,13 +1,25 @@
+import { useNavigate } from "react-router-dom";
 import SelectedFacilityService from "../../services/SelectedFacilityService";
 import "./Dashboard.css";
 import DashboardSidePanel from "./DashboardSidePanel";
 import FloorMapDisplay from "./FloorMapDisplay";
+import FullPageNotification from "../common/FullPageNotification";
 
 function Dashboard() {
 	const selectedFacility = SelectedFacilityService.getSelectedFacility();
+	const navigate = useNavigate();
 
 	if (!selectedFacility) {
-		return <div className="content">No facility found.</div>;
+		return (
+			<FullPageNotification
+				title=""
+				message="No facilities could be found."
+				buttonLabel="Go to Facilties Manager"
+				onButtonClick={() => {
+					navigate("/facilities");
+				}}
+			/>
+		);
 	}
 
 	return (
