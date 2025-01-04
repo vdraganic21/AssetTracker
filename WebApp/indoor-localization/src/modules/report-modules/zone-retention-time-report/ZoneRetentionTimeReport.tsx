fd05ff92bbb77cc89498e80748986220bf55317b
Ariana Slaviček <aslavicek21@foi.hr>
2025-01-04 22:06:31 +0100

    Added basic design to Zone Retention Time report. Commented out max width and height in App css file due to bugs.

--- WebApp/indoor-localization/src/modules/report-modules/zone-retention-time-report/ZoneRetentionTimeReport.tsx
+++ WebApp/indoor-localization/src/modules/report-modules/zone-retention-time-report/ZoneRetentionTimeReport.tsx
@@ -1,17 +1,9 @@
 import { useState } from "react";
-import {
-  TextField,
-  MenuItem,
-  Button,
-  Typography,
-  Card,
-  CardHeader,
-  CardContent,
-} from "@mui/material";
 import DatePicker from "react-datepicker";
 import { Container, Row, Col } from "react-bootstrap";
 import "react-datepicker/dist/react-datepicker.css";
 import "./ZoneRetentionTimeReport.css";
+import { SynButton, SynOption, SynSelect } from "@synergy-design-system/react";
 
 function ZoneRetentionTimeReport() {
   const [facility, setFacility] = useState("");
@@ -95,106 +87,45 @@ function ZoneRetentionTimeReport() {
             <Row>
               <Container fluid="true">
                 <div className="retention-panel">
-                  <Typography variant="h6" className="panel-title">
-                    Retention Report
-                  </Typography>
+                  <span className="panel-title">Retention Report</span>
                   <div className="input-group">
-                    <Typography variant="subtitle1" className="input-label">
-                      Facility
-                    </Typography>
-                    <TextField
-                      select
-                      label="Select Facility"
-                      value={facility}
-                      onChange={(e) => setFacility(e.target.value)}
-                      fullWidth
-                      className="facility-dropdown"
-                    >
-                      <MenuItem value="">Select Facility</MenuItem>
-                      <MenuItem value="Facility 1">Facility 1</MenuItem>
-                      <MenuItem value="Facility 2">Facility 2</MenuItem>
-                    </TextField>
+                    <span className="input-label">Facility</span>
+                    <SynSelect value={"option1"} className="sort-select">
+                      <SynOption value={"Facility 1"}>{"Facility1"}</SynOption>
+                    </SynSelect>
                   </div>
                   <div className="input-group">
-                    <Typography variant="subtitle1" className="input-label">
-                      Zone
-                    </Typography>
-                    <TextField
-                      select
-                      label="Select Zone"
-                      value={zone}
-                      onChange={(e) => setZone(e.target.value)}
-                      fullWidth
-                      className="zone-dropdown"
-                    >
-                      <MenuItem value="">Select Zone</MenuItem>
-                      <MenuItem value="Zone 1">Zone 1</MenuItem>
-                      <MenuItem value="Zone 2">Zone 2</MenuItem>
-                    </TextField>
+                    <span className="input-label">Zone</span>
+                    <SynSelect value={"option1"} className="sort-select">
+                      <SynOption value={"Facility 1"}>{"Facility1"}</SynOption>
+                    </SynSelect>
                   </div>
                   <div className="input-group">
-                    <Typography variant="subtitle1" className="input-label">
-                      Retention Threshold
-                    </Typography>
+                    <span className="input-label">Retention Threshold</span>
                     <div className="picker-row">
-                      <TextField
-                        select
-                        value={threshold.split(":")[0]}
-                        onChange={(e) =>
-                          handleThresholdChange({
-                            value: e.target.value,
-                            isHour: true,
-                          })
-                        }
-                        className="time-picker"
-                      >
-                        {hourOptions.map((hour) => (
-                          <MenuItem key={hour} value={hour}>
-                            {hour}
-                          </MenuItem>
-                        ))}
-                      </TextField>
+                      <SynSelect value={"option1"} className="sort-select">
+                        <SynOption value={"Facility 1"}>
+                          {"Facility1"}
+                        </SynOption>
+                      </SynSelect>
                       :
-                      <TextField
-                        select
-                        value={threshold.split(":")[1]}
-                        onChange={(e) =>
-                          handleThresholdChange({
-                            value: e.target.value,
-                            isHour: false,
-                          })
-                        }
-                        className="time-picker"
-                      >
-                        {minuteOptions.map((minute) => (
-                          <MenuItem key={minute} value={minute}>
-                            {minute}
-                          </MenuItem>
-                        ))}
-                      </TextField>
+                      <SynSelect value={"option1"} className="sort-select">
+                        <SynOption value={"Facility 1"}>
+                          {"Facility1"}
+                        </SynOption>
+                      </SynSelect>
                     </div>
                   </div>
                   <div className="time-span-container">
                     <div className="input-group">
-                      <Typography variant="subtitle1" className="input-label">
-                        Time Span
-                      </Typography>
-                      <TextField
-                        select
-                        value={timeSpan}
-                        onChange={(e) => setTimeSpan(e.target.value)}
-                        fullWidth
-                        className="time-span-dropdown"
-                      >
-                        <MenuItem value="custom">Custom</MenuItem>
-                        <MenuItem value="last day">Last Day</MenuItem>
-                        <MenuItem value="last week">Last Week</MenuItem>
-                        <MenuItem value="last month">Last Month</MenuItem>
-                      </TextField>
+                      <span className="input-label">Time Span</span>
+                      <SynSelect value={"option1"} className="sort-select">
+                        <SynOption value={"Facility 1"}>
+                          {"Facility1"}
+                        </SynOption>
+                      </SynSelect>
                     </div>
-                    <Typography variant="subtitle1" className="input-label">
-                      From
-                    </Typography>
+                    <span className="input-label">From</span>
                     <div className="picker-row">
                       <DatePicker
                         selected={customRange.from}
@@ -204,47 +135,19 @@ function ZoneRetentionTimeReport() {
                         placeholderText="Select Date"
                         className="date-picker"
                       />
-                      <TextField
-                        select
-                        value={customRange.fromTime.split(":")[0]}
-                        onChange={(e) =>
-                          handleCustomTimeChange({
-                            value: e.target.value,
-                            isFrom: true,
-                            isHour: true,
-                          })
-                        }
-                        className="time-picker"
-                      >
-                        {hourOptions.map((hour) => (
-                          <MenuItem key={hour} value={hour}>
-                            {hour}
-                          </MenuItem>
-                        ))}
-                      </TextField>
+                      <SynSelect value={"option1"} className="sort-select">
+                        <SynOption value={"Facility 1"}>
+                          {"Facility1"}
+                        </SynOption>
+                      </SynSelect>
                       :
-                      <TextField
-                        select
-                        value={customRange.fromTime.split(":")[1]}
-                        onChange={(e) =>
-                          handleCustomTimeChange({
-                            value: e.target.value,
-                            isFrom: true,
-                            isHour: false,
-                          })
-                        }
-                        className="time-picker"
-                      >
-                        {minuteOptions.map((minute) => (
-                          <MenuItem key={minute} value={minute}>
-                            {minute}
-                          </MenuItem>
-                        ))}
-                      </TextField>
+                      <SynSelect value={"option1"} className="sort-select">
+                        <SynOption value={"Facility 1"}>
+                          {"Facility1"}
+                        </SynOption>
+                      </SynSelect>
                     </div>
-                    <Typography variant="subtitle1" className="input-label">
-                      To
-                    </Typography>
+                    <span className="input-label">To</span>
                     <div className="picker-row">
                       <DatePicker
                         selected={customRange.to}
@@ -254,60 +157,32 @@ function ZoneRetentionTimeReport() {
                         placeholderText="Select Date"
                         className="date-picker"
                       />
-                      <TextField
-                        select
-                        value={customRange.toTime.split(":")[0]}
-                        onChange={(e) =>
-                          handleCustomTimeChange({
-                            value: e.target.value,
-                            isFrom: false,
-                            isHour: true,
-                          })
-                        }
-                        className="time-picker"
-                      >
-                        {hourOptions.map((hour) => (
-                          <MenuItem key={hour} value={hour}>
-                            {hour}
-                          </MenuItem>
-                        ))}
-                      </TextField>
+                      <SynSelect value={"option1"} className="sort-select">
+                        <SynOption value={"Facility 1"}>
+                          {"Facility1"}
+                        </SynOption>
+                      </SynSelect>
                       :
-                      <TextField
-                        select
-                        value={customRange.toTime.split(":")[1]}
-                        onChange={(e) =>
-                          handleCustomTimeChange({
-                            value: e.target.value,
-                            isFrom: false,
-                            isHour: false,
-                          })
-                        }
-                        className="time-picker"
-                      >
-                        {minuteOptions.map((minute) => (
-                          <MenuItem key={minute} value={minute}>
-                            {minute}
-                          </MenuItem>
-                        ))}
-                      </TextField>
+                      <SynSelect value={"option1"} className="sort-select">
+                        <SynOption value={"Facility 1"}>
+                          {"Facility1"}
+                        </SynOption>
+                      </SynSelect>
                     </div>
                   </div>
-                  <div className="button-group">
-                    <Button
-                      variant="contained"
+                  <div className="SynButton-group">
+                    <SynButton
                       onClick={resetInputs}
-                      className="reset-button"
+                      className="reset-SynButton"
                     >
                       RESET
-                    </Button>
-                    <Button
-                      variant="contained"
-                      onClick={() => console.log("Apply button clicked.")}
-                      className="apply-button"
+                    </SynButton>
+                    <SynButton
+                      onClick={() => console.log("Apply SynButton clicked.")}
+                      className="apply-SynButton"
                     >
                       APPLY
-                    </Button>
+                    </SynButton>
                   </div>
                 </div>
               </Container>
@@ -322,50 +197,22 @@ function ZoneRetentionTimeReport() {
             <Row>
               <Col>
                 <Container fluid="true">
-                  <Card variant="outlined">
-                    <CardHeader title="Average Retention Time" />
-                    <CardContent>
-                      <Typography variant="h6" className="card-value">
-                        00:00
-                      </Typography>
-                    </CardContent>
-                  </Card>
+                  <div className="content-border">blah</div>
                 </Container>
               </Col>
               <Col>
                 <Container fluid="true">
-                  <Card variant="outlined">
-                    <CardHeader title="Max Retention Time" />
-                    <CardContent>
-                      <Typography variant="h6" className="card-value">
-                        00:00
-                      </Typography>
-                    </CardContent>
-                  </Card>
+                  <div className="content-border">blah</div>
                 </Container>
               </Col>
               <Col>
                 <Container fluid="true">
-                  <Card variant="outlined">
-                    <CardHeader title="Min Retention Time" />
-                    <CardContent>
-                      <Typography variant="h6" className="card-value">
-                        00:00
-                      </Typography>
-                    </CardContent>
-                  </Card>
+                  <div className="content-border">blah</div>
                 </Container>
               </Col>
               <Col>
                 <Container fluid="true">
-                  <Card variant="outlined">
-                    <CardHeader title="Assets by retention time" />
-                    <CardContent>
-                      <Typography variant="h6" className="card-value">
-                        00:00
-                      </Typography>
-                    </CardContent>
-                  </Card>
+                  <div className="content-border">blah</div>
                 </Container>
               </Col>
             </Row>
