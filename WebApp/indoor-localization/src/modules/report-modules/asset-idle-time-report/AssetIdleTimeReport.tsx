e2620f54c907d557048f10fd8098e4da0d8dd94a
Ariana Slaviček <aslavicek21@foi.hr>
2025-01-04 23:11:08 +0100

    Made asset idle time report skeleton.

--- WebApp/indoor-localization/src/modules/report-modules/asset-idle-time-report/AssetIdleTimeReport.tsx
+++ WebApp/indoor-localization/src/modules/report-modules/asset-idle-time-report/AssetIdleTimeReport.tsx
@@ -1,9 +1,46 @@
+import { AssetService } from "../../../services/AssetService";
+
 function AssetIdleTimeReport() {
-	return (
-		<div>
-			<h1>Asset Idle Time Report</h1>
-		</div>
-	);
+  const assets = AssetService.GetAll();
+
+  return (
+    <div className="asset-idle-time-report-container">
+      <div className="asset-idle-time-panel">
+        <div>
+          <p>Asset Idle Time Report</p>
+        </div>
+        <table className="syn-table--default">
+          <thead>
+            <tr>
+              <th className="syn-table-cell--shadow-bottom shadow-cell">
+                Name
+              </th>
+              <th className="syn-table-cell--shadow-bottom shadow-cell">
+                Facility
+              </th>
+              <th className="syn-table-cell--shadow-bottom shadow-cell">
+                Zone
+              </th>
+              <th className="syn-table-cell--shadow-bottom shadow-cell"></th>
+            </tr>
+          </thead>
+          <tbody>
+            {assets.map((asset, index) => (
+              <tr className="row-bottom-border hover-row" key={index}>
+                <td>{asset.name}</td>
+                <td>{asset.GetCurrentFacility()?.name ?? "-"}</td>
+                <td>{asset.GetCurrentZones()[0]?.name ?? "-"}</td>
+                <td className="icon-cell"></td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <div>
+        <p>Asset idle time reports -- other components go here</p>
+      </div>
+    </div>
+  );
 }
 
 export default AssetIdleTimeReport;
