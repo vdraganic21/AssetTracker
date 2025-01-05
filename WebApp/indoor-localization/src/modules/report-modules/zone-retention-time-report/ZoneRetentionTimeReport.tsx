acd9dbde2cf3969664c48a5d03fc7e8902173e11
vdraganic21 <vdraganic21@student.foi.hr>
2025-01-05 02:24:20 +0100

    Refactored ZoneRetentionTimeReport. Added data widgets.

--- WebApp/indoor-localization/src/modules/report-modules/zone-retention-time-report/ZoneRetentionTimeReport.tsx
+++ WebApp/indoor-localization/src/modules/report-modules/zone-retention-time-report/ZoneRetentionTimeReport.tsx
@@ -1,239 +1,219 @@
 import { useState } from "react";
 import DatePicker from "react-datepicker";
-import { Container, Row, Col } from "react-bootstrap";
 import "react-datepicker/dist/react-datepicker.css";
 import "./ZoneRetentionTimeReport.css";
 import { SynButton, SynOption, SynSelect } from "@synergy-design-system/react";
-// test
+import "../../../components/common/Report.css";
+import DataComparisonReportWidget from "../../../components/common/DataComparisonReportWidget";
 
 function ZoneRetentionTimeReport() {
-  const [facility, setFacility] = useState("");
-  const [zone, setZone] = useState("");
-  const [threshold, setThreshold] = useState("00:00");
-  const [timeSpan, setTimeSpan] = useState("custom");
-  const [customRange, setCustomRange] = useState<{
-    from: Date | null;
-    fromTime: string;
-    to: Date | null;
-    toTime: string;
-  }>({
-    from: null,
-    fromTime: "00:00",
-    to: null,
-    toTime: "00:00",
-  });
+	const [facility, setFacility] = useState("");
+	const [zone, setZone] = useState("");
+	const [threshold, setThreshold] = useState("00:00");
+	const [timeSpan, setTimeSpan] = useState("custom");
+	const [customRange, setCustomRange] = useState<{
+		from: Date | null;
+		fromTime: string;
+		to: Date | null;
+		toTime: string;
+	}>({
+		from: null,
+		fromTime: "00:00",
+		to: null,
+		toTime: "00:00",
+	});
 
-  const generateDigitOptions = (max: number): string[] => {
-    const options: string[] = [];
-    for (let i = 0; i <= max; i++) {
-      options.push(i.toString().padStart(2, "0"));
-    }
-    return options;
-  };
+	const generateDigitOptions = (max: number): string[] => {
+		const options: string[] = [];
+		for (let i = 0; i <= max; i++) {
+			options.push(i.toString().padStart(2, "0"));
+		}
+		return options;
+	};
 
-  interface CustomTimeChangeParams {
-    value: string;
-    isFrom: boolean;
-    isHour: boolean;
-  }
+	interface CustomTimeChangeParams {
+		value: string;
+		isFrom: boolean;
+		isHour: boolean;
+	}
 
-  const handleCustomTimeChange = ({
-    value,
-    isFrom,
-    isHour,
-  }: CustomTimeChangeParams) => {
-    const key = isFrom ? "fromTime" : "toTime";
-    const currentTime = customRange[key].split(":");
-    const newTime = isHour
-      ? `${value}:${currentTime[1]}`
-      : `${currentTime[0]}:${value}`;
+	const handleCustomTimeChange = ({
+		value,
+		isFrom,
+		isHour,
+	}: CustomTimeChangeParams) => {
+		const key = isFrom ? "fromTime" : "toTime";
+		const currentTime = customRange[key].split(":");
+		const newTime = isHour
+			? `${value}:${currentTime[1]}`
+			: `${currentTime[0]}:${value}`;
 
-    setCustomRange({ ...customRange, [key]: newTime });
-  };
+		setCustomRange({ ...customRange, [key]: newTime });
+	};
 
-  interface ThresholdChangeParams {
-    value: string;
-    isHour: boolean;
-  }
+	interface ThresholdChangeParams {
+		value: string;
+		isHour: boolean;
+	}
 
-  const handleThresholdChange = ({ value, isHour }: ThresholdChangeParams) => {
-    const currentTime = threshold.split(":");
-    const newTime = isHour
-      ? `${value}:${currentTime[1]}`
-      : `${currentTime[0]}:${value}`;
-    setThreshold(newTime);
-  };
+	const handleThresholdChange = ({ value, isHour }: ThresholdChangeParams) => {
+		const currentTime = threshold.split(":");
+		const newTime = isHour
+			? `${value}:${currentTime[1]}`
+			: `${currentTime[0]}:${value}`;
+		setThreshold(newTime);
+	};
 
-  const resetInputs = () => {
-    setFacility("");
-    setZone("");
-    setThreshold("00:00");
-    setTimeSpan("custom");
-    setCustomRange({
-      from: null,
-      fromTime: "00:00",
-      to: null,
-      toTime: "00:00",
-    });
-  };
+	const resetInputs = () => {
+		setFacility("");
+		setZone("");
+		setThreshold("00:00");
+		setTimeSpan("custom");
+		setCustomRange({
+			from: null,
+			fromTime: "00:00",
+			to: null,
+			toTime: "00:00",
+		});
+	};
 
-  const hourOptions = generateDigitOptions(23);
-  const minuteOptions = generateDigitOptions(59);
+	const hourOptions = generateDigitOptions(23);
+	const minuteOptions = generateDigitOptions(59);
 
-  return (
-    <Container fluid="true">
-      <Row>
-        <Col md="auto">
-          <Container>
-            <Row>
-              <Container fluid="true">
-                <div className="retention-panel">
-                  <span className="panel-title">Retention Report</span>
-                  <div className="input-group">
-                    <span className="input-label">Facility</span>
-                    <SynSelect value={"option1"} className="sort-select">
-                      <SynOption value={"Facility 1"}>{"Facility1"}</SynOption>
-                    </SynSelect>
-                  </div>
-                  <div className="input-group">
-                    <span className="input-label">Zone</span>
-                    <SynSelect value={"option1"} className="sort-select">
-                      <SynOption value={"Facility 1"}>{"Facility1"}</SynOption>
-                    </SynSelect>
-                  </div>
-                  <div className="input-group">
-                    <span className="input-label">Retention Threshold</span>
-                    <div className="picker-row">
-                      <SynSelect value={"option1"} className="sort-select">
-                        <SynOption value={"Facility 1"}>
-                          {"Facility1"}
-                        </SynOption>
-                      </SynSelect>
-                      :
-                      <SynSelect value={"option1"} className="sort-select">
-                        <SynOption value={"Facility 1"}>
-                          {"Facility1"}
-                        </SynOption>
-                      </SynSelect>
-                    </div>
-                  </div>
-                  <div className="time-span-container">
-                    <div className="input-group">
-                      <span className="input-label">Time Span</span>
-                      <SynSelect value={"option1"} className="sort-select">
-                        <SynOption value={"Facility 1"}>
-                          {"Facility1"}
-                        </SynOption>
-                      </SynSelect>
-                    </div>
-                    <span className="input-label">From</span>
-                    <div className="picker-row">
-                      <DatePicker
-                        selected={customRange.from}
-                        onChange={(date) =>
-                          setCustomRange({ ...customRange, from: date })
-                        }
-                        placeholderText="Select Date"
-                        className="date-picker"
-                      />
-                      <SynSelect value={"option1"} className="sort-select">
-                        <SynOption value={"Facility 1"}>
-                          {"Facility1"}
-                        </SynOption>
-                      </SynSelect>
-                      :
-                      <SynSelect value={"option1"} className="sort-select">
-                        <SynOption value={"Facility 1"}>
-                          {"Facility1"}
-                        </SynOption>
-                      </SynSelect>
-                    </div>
-                    <span className="input-label">To</span>
-                    <div className="picker-row">
-                      <DatePicker
-                        selected={customRange.to}
-                        onChange={(date) =>
-                          setCustomRange({ ...customRange, to: date })
-                        }
-                        placeholderText="Select Date"
-                        className="date-picker"
-                      />
-                      <SynSelect value={"option1"} className="sort-select">
-                        <SynOption value={"Facility 1"}>
-                          {"Facility1"}
-                        </SynOption>
-                      </SynSelect>
-                      :
-                      <SynSelect value={"option1"} className="sort-select">
-                        <SynOption value={"Facility 1"}>
-                          {"Facility1"}
-                        </SynOption>
-                      </SynSelect>
-                    </div>
-                  </div>
-                  <div className="SynButton-group">
-                    <SynButton
-                      onClick={resetInputs}
-                      className="reset-SynButton"
-                    >
-                      RESET
-                    </SynButton>
-                    <SynButton
-                      onClick={() => console.log("Apply SynButton clicked.")}
-                      className="apply-SynButton"
-                    >
-                      APPLY
-                    </SynButton>
-                  </div>
-                </div>
-              </Container>
-            </Row>
-            <Row>
-              <Container fluid="true">Empty export</Container>
-            </Row>
-          </Container>
-        </Col>
-        <Col>
-          <Container fluid="true">
-            <Row>
-              <Col>
-                <Container fluid="true">
-                  <div className="content-border">blah</div>
-                </Container>
-              </Col>
-              <Col>
-                <Container fluid="true">
-                  <div className="content-border">blah</div>
-                </Container>
-              </Col>
-              <Col>
-                <Container fluid="true">
-                  <div className="content-border">blah</div>
-                </Container>
-              </Col>
-              <Col>
-                <Container fluid="true">
-                  <div className="content-border">blah</div>
-                </Container>
-              </Col>
-            </Row>
-            <Row>
-              <Col>
-                <Container fluid="true">
-                  <div className="content-border">Blah</div>
-                </Container>
-              </Col>
-              <Col>
-                <Container fluid="true">
-                  <div className="content-border">Blah</div>
-                </Container>
-              </Col>
-            </Row>
-          </Container>
-        </Col>
-      </Row>
-    </Container>
-  );
+	return (
+		<div className="report-row take-space report-padding">
+			<div className="report-column">
+				<div className="retention-panel">
+					<span className="panel-title">Retention Report</span>
+					<div className="input-group">
+						<span className="input-label">Facility</span>
+						<SynSelect value={"option1"} className="sort-select">
+							<SynOption value={"Facility 1"}>{"Facility1"}</SynOption>
+						</SynSelect>
+					</div>
+					<div className="input-group">
+						<span className="input-label">Zone</span>
+						<SynSelect value={"option1"} className="sort-select">
+							<SynOption value={"Facility 1"}>{"Facility1"}</SynOption>
+						</SynSelect>
+					</div>
+					<div className="input-group">
+						<span className="input-label">Retention Threshold</span>
+						<div className="picker-row">
+							<SynSelect value={"option1"} className="sort-select">
+								<SynOption value={"Facility 1"}>{"Facility1"}</SynOption>
+							</SynSelect>
+							:
+							<SynSelect value={"option1"} className="sort-select">
+								<SynOption value={"Facility 1"}>{"Facility1"}</SynOption>
+							</SynSelect>
+						</div>
+					</div>
+					<div className="time-span-container">
+						<div className="input-group">
+							<span className="input-label">Time Span</span>
+							<SynSelect value={"option1"} className="sort-select">
+								<SynOption value={"Facility 1"}>{"Facility1"}</SynOption>
+							</SynSelect>
+						</div>
+						<span className="input-label">From</span>
+						<div className="picker-row">
+							<DatePicker
+								selected={customRange.from}
+								onChange={(date) =>
+									setCustomRange({ ...customRange, from: date })
+								}
+								placeholderText="Select Date"
+								className="date-picker"
+							/>
+							<SynSelect value={"option1"} className="sort-select">
+								<SynOption value={"Facility 1"}>{"Facility1"}</SynOption>
+							</SynSelect>
+							:
+							<SynSelect value={"option1"} className="sort-select">
+								<SynOption value={"Facility 1"}>{"Facility1"}</SynOption>
+							</SynSelect>
+						</div>
+						<span className="input-label">To</span>
+						<div className="picker-row">
+							<DatePicker
+								selected={customRange.to}
+								onChange={(date) =>
+									setCustomRange({ ...customRange, to: date })
+								}
+								placeholderText="Select Date"
+								className="date-picker"
+							/>
+							<SynSelect value={"option1"} className="sort-select">
+								<SynOption value={"Facility 1"}>{"Facility1"}</SynOption>
+							</SynSelect>
+							:
+							<SynSelect value={"option1"} className="sort-select">
+								<SynOption value={"Facility 1"}>{"Facility1"}</SynOption>
+							</SynSelect>
+						</div>
+					</div>
+					<div className="SynButton-group">
+						<SynButton
+							variant="filled"
+							onClick={resetInputs}
+							className="reset-SynButton"
+						>
+							RESET
+						</SynButton>
+						<SynButton
+							variant="filled"
+							onClick={() => console.log("Apply SynButton clicked.")}
+							className="apply-SynButton"
+						>
+							APPLY
+						</SynButton>
+					</div>
+				</div>
+				<div className="content-border take-space">Export stuff</div>
+			</div>
+			<div className="report-column take-space">
+				<div className="report-row">
+					<DataComparisonReportWidget
+						mainData="17min"
+						mainDescription="Viljuškar idle time last 24h"
+						secondaryDataLeft="15min"
+						secondaryDescriptionLeft="Last week"
+						secondaryDataRight="16min"
+						secondaryDescriptionRight="Last month"
+					/>
+					<DataComparisonReportWidget
+						mainData="12h 15min"
+						mainDescription="Viljuškar idle time last 24h"
+						secondaryDataLeft="15min"
+						secondaryDescriptionLeft="Last week"
+						secondaryDataRight="16min"
+						secondaryDescriptionRight="Last month"
+					/>
+					<DataComparisonReportWidget
+						mainData="17min"
+						mainDescription="Viljuškar idle time last 24h"
+						secondaryDataLeft="12h 15min"
+						secondaryDescriptionLeft="Last week"
+						secondaryDataRight="16min"
+						secondaryDescriptionRight="Last month"
+					/>
+					<DataComparisonReportWidget
+						mainData="17min"
+						mainDescription="Viljuškar idle time last 24h"
+						secondaryDataLeft="15min"
+						secondaryDescriptionLeft="Last week"
+						secondaryDataRight="16min"
+						secondaryDescriptionRight="Last month"
+					/>
+				</div>
+				<div className="report-row take-space">
+					<div className="content-border take-space">blah</div>
+					<div className="content-border take-space">blah</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default ZoneRetentionTimeReport;
