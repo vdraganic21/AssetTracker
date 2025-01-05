510ac2c8d5d7390f87be93da8d35580063614d3e
Ariana Slaviček <aslavicek21@foi.hr>
2025-01-05 19:40:37 +0100

    Edited report export button group.

--- WebApp/indoor-localization/src/components/common/ReportExportButtonGroup.tsx
+++ WebApp/indoor-localization/src/components/common/ReportExportButtonGroup.tsx
@@ -3,15 +3,17 @@ import "./ReportExportButtonGroup.css";
 
 function ReportExportButtonGroup({ className = "" }: { className?: string }) {
   return (
-    <div className={"asset-report-button-group " + className}>
+    <>
       <span className="export-input-label">Export as</span>
-      <SynSelect value={"PDF"} className="sort-select">
-        <SynOption value={"PDF"}>{"PDF"}</SynOption>
-        <SynOption value={"PNG"}>{"PNG"}</SynOption>
-      </SynSelect>
-      <SynButton>Export</SynButton>
-      <SynButton>Print</SynButton>
-    </div>
+      <div className={"asset-report-button-group " + className}>
+        <SynSelect value={"PDF"} className="sort-select">
+          <SynOption value={"PDF"}>{"PDF"}</SynOption>
+          <SynOption value={"PNG"}>{"PNG"}</SynOption>
+        </SynSelect>
+        <SynButton>Export</SynButton>
+        <SynButton>Print</SynButton>
+      </div>
+    </>
   );
 }
 
