38f2bcb408bf31e768dbb3c5d24a7c6e1116636e
vdraganic21 <vdraganic21@student.foi.hr>
2025-01-05 02:21:06 +0100

    Refactored DataComparisonReportWidget to accept props for data.

--- WebApp/indoor-localization/src/components/common/DataComparisonReportWidget.tsx
+++ WebApp/indoor-localization/src/components/common/DataComparisonReportWidget.tsx
@@ -1,20 +1,36 @@
 import { SynDivider } from "@synergy-design-system/react";
 import "./DataComparisonReportWidget.css";
 
-function DataComparisonReportWidget() {
+interface DataComparisonReportWidgetProps {
+	mainData: string;
+	mainDescription: string;
+	secondaryDataLeft: string;
+	secondaryDataRight: string;
+	secondaryDescriptionLeft: string;
+	secondaryDescriptionRight: string;
+}
+
+function DataComparisonReportWidget({
+	mainData,
+	mainDescription,
+	secondaryDataLeft,
+	secondaryDataRight,
+	secondaryDescriptionLeft,
+	secondaryDescriptionRight,
+}: DataComparisonReportWidgetProps) {
 	return (
 		<div className="content-border widget">
-			<p className="main-data">35min</p>
-			<p className="description-text">Viljuškar idle time last 24h</p>
+			<p className="main-data">{mainData}</p>
+			<p className="description-text">{mainDescription}</p>
 			<SynDivider className="divider" />
 			<div className="bottom-data-container">
 				<div>
-					<p className="secondary-data">17 min</p>
-					<p className="description-text">Last week</p>
+					<p className="secondary-data">{secondaryDataLeft}</p>
+					<p className="description-text">{secondaryDescriptionLeft}</p>
 				</div>
 				<div>
-					<p className="secondary-data">16 min</p>
-					<p className="description-text">Last month</p>
+					<p className="secondary-data">{secondaryDataRight}</p>
+					<p className="description-text">{secondaryDescriptionRight}</p>
 				</div>
 			</div>
 		</div>
