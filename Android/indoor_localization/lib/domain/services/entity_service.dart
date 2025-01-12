32193b04851cb8dfac127e26f4fb4857177c0e4b
mmathis21 <mmathis21@student.foi.hr>
2025-01-12 20:39:43 +0100

    Removed mock repositories and connected the services to real repositories.

--- Android/indoor_localization/lib/domain/services/entity_service.dart
+++ Android/indoor_localization/lib/domain/services/entity_service.dart
@@ -1,12 +1,17 @@
-import 'package:indoor_localization/domain/mock-repositories/mock_asset_repository.dart';
-import 'package:indoor_localization/domain/mock-repositories/mock_facility_repository.dart';
+import 'package:indoor_localization/data/datasources/remote/facility_remote_datasource.dart';
+import 'package:indoor_localization/data/repositories/facility_repository.dart';
 import 'package:indoor_localization/domain/mock-repositories/mock_zone_repository.dart';
 import 'package:indoor_localization/domain/repository-interfaces/asset_repository.dart';
 import 'package:indoor_localization/domain/repository-interfaces/facility_repository.dart';
 import 'package:indoor_localization/domain/repository-interfaces/zone_repository.dart';
+import 'package:http/http.dart' as http;
+import 'package:indoor_localization/data/datasources/remote/asset_remote_datasource.dart';
+import 'package:indoor_localization/data/repositories/asset_repository.dart';
+
 
 abstract class EntityService {
-  static final IAssetRepository assetRepo = MockAssetRepository();
+  static final IAssetRepository assetRepo = AssetRepository(AssetRemoteDataSource(http.Client()));
+  static final IFacilityRepository facilityRepo = FacilityRepository(FacilityRemoteDataSource(client: http.Client()));
   static final IZoneRepository zoneRepo = MockZoneRepository();
-  static final IFacilityRepository facilityRepo = MockFacilityRepository();
 }
+
