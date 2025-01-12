21517754e8f783cba81d4fc8f4e07102f7b66e57
vdraganic21 <vdraganic21@student.foi.hr>
2025-01-12 19:39:19 +0100

    Removed unused import.

--- WebApp/indoor-localization/src/components/dashboard/Dashboard.tsx
+++ WebApp/indoor-localization/src/components/dashboard/Dashboard.tsx
@@ -1,6 +1,5 @@
 import { useNavigate } from "react-router-dom";
 import SelectedFacilityService from "../../services/SelectedFacilityService";
-import "./Dashboard.css";
 import DashboardSidePanel from "./DashboardSidePanel";
 import FloorMapDisplay from "./FloorMapDisplay";
 import FullPageNotification from "../common/FullPageNotification";
