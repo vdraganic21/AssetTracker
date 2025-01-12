575848adc5dc10641a23050504b8391abb17887c
mmathis21 <mmathis21@student.foi.hr>
2025-01-12 20:47:23 +0100

    Added FacilityRemoteDataSource for fetching facilities from API

---
+++ Android/indoor_localization/lib/data/datasources/remote/facility_remote_datasource.dart
@@ -0,0 +1,23 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:indoor_localization/domain/entities/facility.dart';
+import 'package:indoor_localization/data/connections/api.dart';
+
+
+class FacilityRemoteDataSource {
+  final http.Client client;
+
+  FacilityRemoteDataSource({required this.client});
+
+  // Fetch facilities from the API
+  Future<List<Facility>> fetchFacilities() async {
+    final response = await http.get(Uri.parse('${api.apiUrl}/floorMaps'));
+
+    if (response.statusCode == 200) {
+      final List<dynamic> jsonData = json.decode(response.body);
+      return jsonData.map((json) => Facility.fromJson(json)).toList();
+    } else {
+      throw Exception('Failed to load facilities');
+    }
+  }
+}
