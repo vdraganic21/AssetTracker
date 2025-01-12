38439238d53a2542b0df1ab2863a9d6840fe0cca
mmathis21 <mmathis21@student.foi.hr>
2025-01-12 20:31:35 +0100

    Implemented fetchAssets(), returns all assets from /assets endpoint

---
+++ Android/indoor_localization/lib/data/datasources/remote/asset_remote_datasource.dart
@@ -0,0 +1,20 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:indoor_localization/data/connections/api.dart';
+
+class AssetRemoteDataSource {
+  final http.Client client;
+
+  AssetRemoteDataSource(this.client);
+
+  Future<List<Map<String, dynamic>>> fetchAssets() async {
+    final response = await client.get(Uri.parse('${api.apiUrl}/assets'));
+
+    if (response.statusCode == 200) {
+      final List<dynamic> jsonData = json.decode(response.body);
+      return jsonData.map((json) => json as Map<String, dynamic>).toList();
+    } else {
+      throw Exception("Failed to load assets");
+    }
+  }
+}
