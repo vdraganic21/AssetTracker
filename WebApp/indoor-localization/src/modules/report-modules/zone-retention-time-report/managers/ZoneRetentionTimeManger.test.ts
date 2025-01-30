0878480cf453d48a2b53f11e708aa76f2e85cfe0
vdraganic21 <vdraganic21@student.foi.hr>
2025-01-30 20:40:08 +0100

    Added tests for getAssetRetentionTime method.

--- WebApp/indoor-localization/src/modules/report-modules/zone-retention-time-report/managers/ZoneRetentionTimeManger.test.ts
+++ WebApp/indoor-localization/src/modules/report-modules/zone-retention-time-report/managers/ZoneRetentionTimeManger.test.ts
@@ -59,5 +59,71 @@ describe('ZoneRetentionTimeManager', () => {
         expect(assetIds).toEqual(expectedAssetIds);
     });
 
-    
+    it.each([
+        {
+            description: 'should return 0 if getting asset retention time from empty dataset',
+            dataset: [],
+            id: 1,
+            expectedRetentionTime: 0,
+        },
+        {
+            description: 'should return 0 if getting asset retention time from dataset with no logs for asset',
+            dataset: [
+                new AssetZoneHistoryLog(1, 3, 1, new Date(), new Date(), 5),
+                new AssetZoneHistoryLog(2, 2, 1, new Date(), new Date(), 5),
+                new AssetZoneHistoryLog(3, 3, 1, new Date(), new Date(), 5),
+                new AssetZoneHistoryLog(4, 1, 1, new Date(), new Date(), 5),
+                new AssetZoneHistoryLog(5, 2, 1, new Date(), new Date(), 5),
+            ],
+            id: 22,
+            expectedRetentionTime: 0,
+        },
+        {
+            description: 'should return retention time if getting asset retention time from dataset with log for asset',
+            dataset: [
+                new AssetZoneHistoryLog(1, 3, 1, new Date(), new Date(), 5),
+            ],
+            id: 3,
+            expectedRetentionTime: 5,
+        },
+        {
+            description: 'should return sum of retention times if getting asset retention time from dataset with multiple logs for asset',
+            dataset: [
+                new AssetZoneHistoryLog(1, 3, 1, new Date(), new Date(), 5),
+                new AssetZoneHistoryLog(1, 3, 1, new Date(), new Date(), 15),
+            ],
+            id: 3,
+            expectedRetentionTime: 20,
+        },
+        {
+            description: 'should return sum of retention times if getting asset retention time from dataset with multiple logs for different asset',
+            dataset: [
+                new AssetZoneHistoryLog(1, 3, 1, new Date(), new Date(), 5),
+                new AssetZoneHistoryLog(1, 2, 1, new Date(), new Date(), 5),
+                new AssetZoneHistoryLog(1, 3, 1, new Date(), new Date(), 15),
+                new AssetZoneHistoryLog(1, 1, 1, new Date(), new Date(), 5),
+                new AssetZoneHistoryLog(1, 4, 1, new Date(), new Date(), 45),
+                new AssetZoneHistoryLog(1, 3, 1, new Date(), new Date(), 5),
+            ],
+            id: 3,
+            expectedRetentionTime: 25,
+        },
+        {
+            description: 'should return decimal sum of retention times if getting asset retention time from dataset with multiple logs for different asset',
+            dataset: [
+                new AssetZoneHistoryLog(1, 3, 1, new Date(), new Date(), 0.5),
+                new AssetZoneHistoryLog(1, 2, 1, new Date(), new Date(), 5),
+                new AssetZoneHistoryLog(1, 3, 1, new Date(), new Date(), 1.5),
+                new AssetZoneHistoryLog(1, 1, 1, new Date(), new Date(), 5),
+                new AssetZoneHistoryLog(1, 4, 1, new Date(), new Date(), 45),
+                new AssetZoneHistoryLog(1, 3, 1, new Date(), new Date(), 0.5),
+            ],
+            id: 3,
+            expectedRetentionTime: 2.5,
+        },
+    ])('$description', ({ dataset, id, expectedRetentionTime }) => {
+        let manager = new ZoneRetentionTimeManager(dataset);
+        let assetRetentionTime = manager.getAssetRetentionTime(id);
+        expect(assetRetentionTime).toEqual(expectedRetentionTime);
+    });
 });
