43aa2e470c382006f1846ac63f9575a5a248ae97
vdraganic21 <vdraganic21@student.foi.hr>
2025-01-31 18:05:18 +0100

    Implemented toggle functionality for assets and zones with visibility icons.

--- WebApp/indoor-localization/src/components/dashboard/side_panel_content/ZonesSidePanelList.tsx
+++ WebApp/indoor-localization/src/components/dashboard/side_panel_content/ZonesSidePanelList.tsx
@@ -1,27 +1,45 @@
+import { useState } from "react";
 import { SynDivider, SynIcon } from "@synergy-design-system/react";
 import { Zone } from "../../../entities/Zone";
 import "./SidePanelList.css";
+import HiddenZoneService from "../../../services/HiddenZoneService";
 
 function ZonesSidePanelList({ zones }: { zones: Zone[] }) {
-	if (zones.length == 0)
+	const [, setHiddenZones] = useState<number[]>(
+		HiddenZoneService.GetHiddenZones()
+	);
+
+	const handleToggleZone = (zoneId: number) => {
+		HiddenZoneService.ToggleZone(zoneId);
+		setHiddenZones(HiddenZoneService.GetHiddenZones());
+	};
+
+	if (zones.length === 0) {
 		return (
 			<div>
 				<p className="no-items-message">No zones found.</p>
 			</div>
 		);
+	}
 
 	return (
 		<>
 			<SynDivider />
 			{zones.map((zone, index) => (
-				<>
-					<div className="side-panel-item-list">
-						<p key={index}>{zone.name}</p>
-						<SynIcon library="fa" name="far-eye"></SynIcon>
-					</div>
-					<SynDivider />
-				</>
+				<div key={index} className="side-panel-item-list">
+					<p>{zone.name}</p>
+					<SynIcon
+						library="fa"
+						name={
+							HiddenZoneService.IsZoneHidden(zone.id)
+								? "far-eye-slash"
+								: "far-eye"
+						}
+						onClick={() => handleToggleZone(zone.id)}
+					/>
+				</div>
 			))}
+			<SynDivider />
 		</>
 	);
 }
