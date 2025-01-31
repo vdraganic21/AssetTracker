6ea4bf0539c010eb164cdabeda0d574ae59bfa2c
vdraganic21 <vdraganic21@student.foi.hr>
2025-01-31 18:46:14 +0100

    Add REST repository for AssetZoneHistoryLog.

---
+++ WebApp/indoor-localization/src/data-access/REST-repositories/RESTAssetPositionHistoryLogRepository.ts
@@ -0,0 +1,36 @@
+import { AssetZoneHistoryLog } from "../../entities/AssetZoneHistoryLog";
+import { AssetZoneHistoryLogFilter } from "../../entities/AssetZoneHistoryLogFilter";
+import { IAssetZoneHistoryLogRepository } from "../repository-interfaces/IAssetZoneHistoryLogRepository";
+
+export class RESTAssetZoneHistoryLogRepository implements IAssetZoneHistoryLogRepository {
+
+  Get(id: number): AssetZoneHistoryLog | null {
+    //TODO: Implement
+
+    return null;
+  }
+
+  GetLogs(assetZoneHistoryLogFilter: AssetZoneHistoryLogFilter): AssetZoneHistoryLog[] {
+    //TODO: Implement
+    return [];
+
+  }
+
+  GetAll(): AssetZoneHistoryLog[] {
+    //TODO: Implement
+
+    return [];
+  }
+
+  Add(log: AssetZoneHistoryLog): boolean {
+    return false;
+  }
+
+  Delete(id: number): boolean {
+    return false;
+  }
+
+  Update(updatedLog: AssetZoneHistoryLog): boolean {
+    return false;
+  }
+}
