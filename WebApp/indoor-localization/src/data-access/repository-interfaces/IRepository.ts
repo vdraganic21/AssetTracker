abfc4694467a29dac1db5274b69439e959649e4d
vdraganic21 <vdraganic21@student.foi.hr>
2025-01-31 21:41:18 +0100

    Refactored IRepository interface to use Promises for asynchronous operations.

--- WebApp/indoor-localization/src/data-access/repository-interfaces/IRepository.ts
+++ WebApp/indoor-localization/src/data-access/repository-interfaces/IRepository.ts
@@ -4,33 +4,33 @@ export interface IRepository<T> {
      * @param id - The ID of the entity.
      * @returns The entity of type T.
      */
-    Get(id: number): T | null;
+    Get(id: number): Promise<T | null>;
   
     /**
      * Gets all entities.
      * @returns A list of entities of type T.
      */
-    GetAll(): T[];
+    GetAll(): Promise<T[]>;
   
     /**
      * Adds a new entity.
      * @param entity - The entity to add.
      * @returns A boolean indicating whether the operation was successful.
      */
-    Add(entity: T): boolean;
+    Add(entity: T): Promise<boolean>;
   
     /**
      * Deletes an entity by its ID.
      * @param id - The ID of the entity to delete.
      * @returns A boolean indicating whether the operation was successful.
      */
-    Delete(id: number): boolean;
+    Delete(id: number): Promise<boolean>;
   
     /**
      * Updates an existing entity.
      * @param entity - The entity to update.
      * @returns A boolean indicating whether the operation was successful.
      */
-    Update(entity: T): boolean;
+    Update(entity: T): Promise<boolean>;
   }
   
