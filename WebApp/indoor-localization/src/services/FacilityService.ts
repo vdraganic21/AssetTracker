fba795f6383ad4137477028e7cd2e8b820d3e4c5
vdraganic21 <vdraganic21@student.foi.hr>
2025-01-31 22:01:55 +0100

    Refactored FacilityService methods to return Promises for asynchronous operations.

--- WebApp/indoor-localization/src/services/FacilityService.ts
+++ WebApp/indoor-localization/src/services/FacilityService.ts
@@ -1,59 +1,25 @@
 import { Facility } from "../entities/Facility";
-import { Asset } from "../entities/Asset";
-import { Zone } from "../entities/Zone";
 import { EntityService } from "./Service";
 
 export class FacilityService extends EntityService {
 
-  static Get(id: number): Facility | null {
+  static async Get(id: number): Promise<Facility | null> {
     return this.facilityRepo.Get(id);
   }
 
-  static GetAll(): Facility[] {
+  static async GetAll(): Promise<Facility[]> {
     return this.facilityRepo.GetAll();
   }
 
-  static Add(facility: Facility): boolean {
+  static async Add(facility: Facility): Promise<boolean> {
     return this.facilityRepo.Add(facility);
   }
 
-  static Delete(id: number): boolean {
+  static async Delete(id: number): Promise<boolean> {
     return this.facilityRepo.Delete(id);
   }
 
-  static Update(facility: Facility): boolean {
+  static async Update(facility: Facility): Promise<boolean> {
     return this.facilityRepo.Update(facility);
-  }
-
-  static GetAssetsInFacility(id: number): Asset[] {
-    const facility = this.facilityRepo.Get(id);
-    if (facility) {
-      return facility.containedAssetsIds.map(assetId => this.assetRepo.Get(assetId)).filter(asset => asset !== null) as Asset[];
-    }
-    return [];
-  }
-
-  static GetZonesInFacility(id: number): Zone[] {
-    const facility = this.facilityRepo.Get(id);
-    if (facility) {
-      return facility.containedZonesIds.map(zoneId => this.zoneRepo.Get(zoneId)).filter(zone => zone !== null) as Zone[];
-    }
-    return [];
-  }
-
-  static FacilityContainsAsset(facilityId: number, assetId: number): boolean {
-    const facility = this.facilityRepo.Get(facilityId);
-    if (facility) {
-      return facility.containedAssetsIds.includes(assetId);
-    }
-    return false;
-  }
-
-  static FacilityContainsZone(facilityId: number, zoneId: number): boolean {
-    const facility = this.facilityRepo.Get(facilityId);
-    if (facility) {
-      return facility.containedZonesIds.includes(zoneId);
-    }
-    return false;
   }
 }
