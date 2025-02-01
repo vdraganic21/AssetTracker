490c5b95cb65846756969b5ba2b2a54f708df2fb
Ariana Slaviček <aslavicek21@foi.hr>
2025-02-01 18:19:26 +0100

    Implemented AssetIdleTimeCounter functions.

--- WebApp/indoor-localization/src/modules/report-modules/zone-retention-time-report/managers/AssetIdleTimeCounter.ts
+++ WebApp/indoor-localization/src/modules/report-modules/zone-retention-time-report/managers/AssetIdleTimeCounter.ts
@@ -1,24 +1,62 @@
 import { AssetPositionHistoryLog } from "../../../../entities/AssetPositionHistoryLog";
 
 export default class AssetIdleTimeCounter {
-    private dataset: AssetPositionHistoryLog[]; 
-    
-    constructor(dataset: AssetPositionHistoryLog[]) {
-        this.dataset = dataset;
+  private dataset: AssetPositionHistoryLog[];
+
+  constructor(dataset: AssetPositionHistoryLog[]) {
+    this.dataset = dataset.sort(
+      (a, b) => a.dateTime.getTime() - b.dateTime.getTime()
+    );
+  }
+
+  public getAssetIdleTime(): number {
+    let totalIdleTime = 0;
+    for (let i = 1; i < this.dataset.length; i++) {
+      if (
+        this.dataset[i - 1].facilityId === this.dataset[i].facilityId &&
+        this.isIdle(this.dataset[i - 1], this.dataset[i])
+      ) {
+        totalIdleTime += this.getTimeDifference(
+          this.dataset[i - 1],
+          this.dataset[i]
+        );
+      }
     }
+    return totalIdleTime;
+  }
 
-    public getAssetIdleTime(): number {
+  public getFacilityIdsInDataset(): number[] {
+    return [...new Set(this.dataset.map((log) => log.facilityId))];
+  }
 
-        return 0;
+  public getIdleTimeInFacility(id: number): number {
+    const facilityLogs = this.dataset.filter((log) => log.facilityId === id);
+    let idleTime = 0;
+    for (let i = 1; i < facilityLogs.length; i++) {
+      if (this.isIdle(facilityLogs[i - 1], facilityLogs[i])) {
+        idleTime += this.getTimeDifference(
+          facilityLogs[i - 1],
+          facilityLogs[i]
+        );
+      }
     }
+    return idleTime;
+  }
 
-    public getFacilityIdsInDataset(): number[] {
+  private isIdle(
+    prevLog: AssetPositionHistoryLog,
+    currentLog: AssetPositionHistoryLog
+  ): boolean {
+    return (
+      prevLog.position.x === currentLog.position.x &&
+      prevLog.position.y === currentLog.position.y
+    );
+  }
 
-        return [];
-    }
-
-    public getIdleTimeInFacility(id: number): number {
-
-        return 0;
-    }
+  private getTimeDifference(
+    prevLog: AssetPositionHistoryLog,
+    currentLog: AssetPositionHistoryLog
+  ): number {
+    return (currentLog.dateTime.getTime() - prevLog.dateTime.getTime()) / 1000;
+  }
 }
