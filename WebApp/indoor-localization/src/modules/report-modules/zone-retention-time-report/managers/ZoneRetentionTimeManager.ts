00a0ca7b89ea365b5d690a7a19e1fd29e8c16399
Ariana Slaviček <aslavicek21@foi.hr>
2025-02-01 17:01:20 +0100

    Implemented functions and completed tests in ZoneRetentionTimeManager.

--- WebApp/indoor-localization/src/modules/report-modules/zone-retention-time-report/managers/ZoneRetentionTimeManager.ts
+++ WebApp/indoor-localization/src/modules/report-modules/zone-retention-time-report/managers/ZoneRetentionTimeManager.ts
@@ -1,32 +1,49 @@
 import { AssetZoneHistoryLog } from "../../../../entities/AssetZoneHistoryLog";
 
 export default class ZoneRetentionTimeManager {
-    private dataset: AssetZoneHistoryLog[]; 
+  private dataset: AssetZoneHistoryLog[];
 
-    constructor(dataset: AssetZoneHistoryLog[]) {
-        this.dataset = dataset;
-    }
+  constructor(dataset: AssetZoneHistoryLog[]) {
+    this.dataset = dataset;
+  }
 
-    public getAssetIdsInDataset(): number[]{
+  public getAssetIdsInDataset(): number[] {
+    return [...new Set(this.dataset.map((log) => log.assetId))];
+  }
 
-        return [];
-    }
-    public getAssetRetentionTime(assetId: number): number{
+  public getAssetRetentionTime(assetId: number): number {
+    return this.dataset
+      .filter((log) => log.assetId === assetId)
+      .reduce((sum, log) => sum + (log.retentionTime || 0), 0);
+  }
 
-        return 0;
-    }
-    public getAvgRetentionTime(): number{
+  public getAvgRetentionTime(): number {
+    if (this.dataset.length === 0) return 0;
 
-        return 0;
-    }
+    const totalRetention = this.dataset.reduce(
+      (sum, log) => sum + (log.retentionTime || 0),
+      0
+    );
+    return totalRetention / this.dataset.length;
+  }
 
-    public getMaxRetentionTime(): number{
+  public getMaxRetentionTime(): number {
+    if (this.dataset.length === 0) return 0;
 
-        return 0;
-    }
+    const assetRetentionTimes = this.getAssetIdsInDataset().map((assetId) =>
+      this.getAssetRetentionTime(assetId)
+    );
 
-    public getMinRetentionTime(): number{
+    return Math.max(...assetRetentionTimes);
+  }
 
-        return 0;
-    }
+  public getMinRetentionTime(): number {
+    if (this.dataset.length === 0) return 0;
+
+    const assetRetentionTimes = this.getAssetIdsInDataset().map((assetId) =>
+      this.getAssetRetentionTime(assetId)
+    );
+
+    return Math.min(...assetRetentionTimes);
+  }
 }
