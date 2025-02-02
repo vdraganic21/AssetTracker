61c5cb169c70e895ecffadcc68cc202bcf232db0
vdraganic21 <vdraganic21@student.foi.hr>
2025-02-02 14:40:25 +0100

    Added asynchronous methods to FacilityService for fetching assets and zones associated with a facility.

--- WebApp/indoor-localization/src/services/FacilityService.ts
+++ WebApp/indoor-localization/src/services/FacilityService.ts
@@ -1,5 +1,9 @@
+import { Asset } from "../entities/Asset";
 import { Facility } from "../entities/Facility";
+import { Zone } from "../entities/Zone";
+import { AssetService } from "./AssetService";
 import { EntityService } from "./Service";
+import { ZoneService } from "./ZoneService";
 
 export class FacilityService extends EntityService {
 
@@ -21,5 +25,27 @@ export class FacilityService extends EntityService {
 
   static async Update(facility: Facility): Promise<boolean> {
     return this.facilityRepo.Update(facility);
+  }
+
+  static async GetFacilityAssets(facility: Facility): Promise<Asset[]> {
+    let assets : Asset[] = [];
+
+    for (let assetId of facility.containedAssetsIds){
+      const asset = await AssetService.Get(assetId);
+      if (asset) assets.push(asset);
+    }
+
+    return assets;
+  }
+
+  static async GetFacilityZones(facility: Facility): Promise<Zone[]> {
+    let zones : Zone[] = [];
+
+    for (let zoneId of facility.containedZonesIds){
+      const zone = await ZoneService.Get(zoneId);
+      if (zone) zones.push(zone);
+    }
+
+    return zones;
   }
 }
