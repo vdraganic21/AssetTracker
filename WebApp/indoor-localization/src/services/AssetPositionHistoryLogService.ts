d74d1bdbd9dd04441433cb35fa4e9c54e56beb36
Ariana Slaviček <aslavicek21@foi.hr>
2025-02-04 17:27:23 +0100

    Refactored AssetPositionHistoryLogFilter to allow nullable properties and updated AssetPositionHistoryLog.

--- WebApp/indoor-localization/src/services/AssetPositionHistoryLogService.ts
+++ WebApp/indoor-localization/src/services/AssetPositionHistoryLogService.ts
@@ -1,28 +1,35 @@
 import { MockAssetPositionHistoryLogRepository } from "../data-access/mock-repositories/MockAssetPositionHistoryLogRepository";
 import { AssetPositionHistoryLog } from "../entities/AssetPositionHistoryLog";
-
+import { AssetPositionHistoryLogFilter } from "../entities/AssetPositionHistoryLogFilter";
 
 export class AssetPositionHistoryLogService {
-    private static logRepo = new MockAssetPositionHistoryLogRepository();
+  private static logRepo = new MockAssetPositionHistoryLogRepository();
 
-    static async Get(id: number): Promise<AssetPositionHistoryLog | null> {
-        return this.logRepo.Get(id);
-      }
+  static async Get(id: number): Promise<AssetPositionHistoryLog | null> {
+    return this.logRepo.Get(id);
+  }
 
-    static async GetAll(): Promise<AssetPositionHistoryLog[]> {
-        return this.logRepo.GetAll();
-    }
+  static async GetAll(): Promise<AssetPositionHistoryLog[]> {
+    return this.logRepo.GetAll();
+  }
 
-    static async Add(log: AssetPositionHistoryLog): Promise<boolean> {
-        return this.logRepo.Add(log);
-    }
+  static async Add(log: AssetPositionHistoryLog): Promise<boolean> {
+    return this.logRepo.Add(log);
+  }
 
-    static async Delete(id: number): Promise<boolean> {
-        return this.logRepo.Delete(id);
-    }
+  static async Delete(id: number): Promise<boolean> {
+    return this.logRepo.Delete(id);
+  }
 
-    static async Update(log: AssetPositionHistoryLog): Promise<boolean> {
-        return this.logRepo.Update(log);
-    }
+  static async Update(log: AssetPositionHistoryLog): Promise<boolean> {
+    return this.logRepo.Update(log);
+  }
 
+  static async GetLogs(
+    filter: AssetPositionHistoryLogFilter
+  ): Promise<AssetPositionHistoryLog[]> {
+    const allLogs = await this.logRepo.GetAll();
+
+    return allLogs;
+  }
 }
