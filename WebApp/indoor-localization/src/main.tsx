e3ff9b9ec306c03b9df0f56361fe58fe00c9d577
vdraganic21 <vdraganic21@student.foi.hr>
2025-02-04 12:17:43 +0100

    Added HeatMapReportModule to the report modules initialization.

--- WebApp/indoor-localization/src/main.tsx
+++ WebApp/indoor-localization/src/main.tsx
@@ -17,30 +17,32 @@ import ReportModulesService from "./services/ReportModulesService.ts";
 import AssetIdleTimeReportModule from "./modules/report-modules/asset-idle-time-report/AssetIdleTimeReportModule.ts";
 import ZoneRetentionTimeReportModule from "./modules/report-modules/zone-retention-time-report/ZoneRetentionTimeReportModule.ts";
 import SpaghettiMapReportModule from "./modules/report-modules/spaghetti-map-report/SpaghettiMapReportModule.ts";
+import HeatMapReportModule from "./modules/report-modules/heatmap-report/HeatmapReportModule.ts";
 
 registerIconLibrary("fa", {
-  resolver: (name) => {
-    const filename = name.replace(/^fa[rbs]-/, "");
-    let folder = "regular";
-    if (name.substring(0, 4) === "fas-") folder = "solid";
-    if (name.substring(0, 4) === "fab-") folder = "brands";
-    return `https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.7.0/svgs/${folder}/${filename}.svg`;
-  },
-  mutator: (svg) => svg.setAttribute("fill", "currentColor"),
+	resolver: (name) => {
+		const filename = name.replace(/^fa[rbs]-/, "");
+		let folder = "regular";
+		if (name.substring(0, 4) === "fas-") folder = "solid";
+		if (name.substring(0, 4) === "fab-") folder = "brands";
+		return `https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.7.0/svgs/${folder}/${filename}.svg`;
+	},
+	mutator: (svg) => svg.setAttribute("fill", "currentColor"),
 });
 
 ReportModulesService.SetModules([
-  new AssetIdleTimeReportModule(),
-  new ZoneRetentionTimeReportModule(),
-  new SpaghettiMapReportModule(),
+	new AssetIdleTimeReportModule(),
+	new ZoneRetentionTimeReportModule(),
+	new SpaghettiMapReportModule(),
+	new HeatMapReportModule(),
 ]);
 
 MockDataInitializer.initializeData(); //Initializes mock data repositories, use for testing and dev purposes
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>
+	<StrictMode>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</StrictMode>
 );
