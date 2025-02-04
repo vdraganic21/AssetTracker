0891edd6c1a75f9a6309840433b4f63a47273321
mmathis21 <mmathis21@student.foi.hr>
2025-02-04 16:28:38 +0100

    Created fromJson method.

--- Android/indoor_localization/lib/domain/entities/asset_position_history_log.dart
+++ Android/indoor_localization/lib/domain/entities/asset_position_history_log.dart
@@ -14,4 +14,14 @@ class AssetPositionHistoryLog {
     required this.position,
     required this.dateTime,
   });
+
+  factory AssetPositionHistoryLog.fromJson(Map<String, dynamic> json) {
+    return AssetPositionHistoryLog(
+      id: json['id'] as int,
+      assetId: json['assetId'] as int,
+      facilityId: json['facilityId'] as int,
+      position: Point.fromJson(json['position'] as Map<String, dynamic>),
+      dateTime: DateTime.parse(json['dateTime'] as String),
+    );
+  }
 }
