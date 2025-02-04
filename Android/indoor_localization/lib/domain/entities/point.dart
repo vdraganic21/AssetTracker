997cd57bd0d12a6b0cce712be3b6c2f25b6c1990
mmathis21 <mmathis21@student.foi.hr>
2025-02-04 16:29:33 +0100

    Added fromJson to Point entity.

--- Android/indoor_localization/lib/domain/entities/point.dart
+++ Android/indoor_localization/lib/domain/entities/point.dart
@@ -3,4 +3,11 @@ class Point {
   final double y;
 
   Point(this.x, this.y);
+
+  factory Point.fromJson(Map<String, dynamic> json) {
+    return Point(
+      (json['x'] as num).toDouble(),
+      (json['y'] as num).toDouble(),
+    );
+  }
 }
