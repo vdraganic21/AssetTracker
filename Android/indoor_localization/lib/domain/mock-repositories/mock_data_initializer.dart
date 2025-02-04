568825e2146fb563ca6415df97fe046de4aa0e3a
mmathis21 <mmathis21@student.foi.hr>
2025-02-04 17:40:04 +0100

    Removed unnecessary code.

--- Android/indoor_localization/lib/domain/mock-repositories/mock_data_initializer.dart
+++ Android/indoor_localization/lib/domain/mock-repositories/mock_data_initializer.dart
@@ -21,16 +21,16 @@ class MockDataInitializer {
           id: 1,
           name: "Warehouse 1",
           imageBase64: floorMapDemo,
-          containedAssetsIds: [1, 2],
-          containedZonesIds: [5],
+          assets: [],
+          zones: [],
         ));
     FacilityService.add(
         Facility(
           id: 2,
           name: "Warehouse 35",
           imageBase64: floorMapDemo,
-          containedAssetsIds: [1, 3],
-          containedZonesIds: [2],
+          assets: [],
+          zones: [],
         )
     );
     FacilityService.add(
@@ -38,8 +38,8 @@ class MockDataInitializer {
           id: 3,
           name: "Production Line",
           imageBase64: floorMapDemo,
-          containedAssetsIds: [],
-          containedZonesIds: [4],
+          assets: [],
+          zones: [],
         )
     );
 
